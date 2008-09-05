de40c9ae235562884fffc71255c8148c40250c3e
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2008-09-05 08:24:59 +0000

    Heel eenvoudige WCF service en client app.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@79 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ poc/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.IService1 service = new ServiceReference1.Service1Client();
+
+            Console.WriteLine(service.GetData(4));
+            Console.WriteLine(service.GetDataUsingDataContract(new ConsoleApplication1.ServiceReference1.CompositeType()));
+
+            Console.ReadLine();
+        }
+    }
+}
