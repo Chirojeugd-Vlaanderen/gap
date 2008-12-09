111e45443ba2a176e85dac2bee80fea0b3d8a01b
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2008-12-09 09:56:01 +0000

    Veel manueel werk voor de mapping.  Nog uit te zoeken wat geautomatiseerd kan worden.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@171 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ poc/EntityFramework/Cg2.Data/Cg2ObjectContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cg2.Core.Domain;
+
+namespace Cg2.Data
+{
+    public class Cg2ObjectContext: global::System.Data.Objects.ObjectContext
+    {
+        #region entiteitcontainers
+        private global::System.Data.Objects.ObjectQuery<Groep> _groepen;
+        private global::System.Data.Objects.ObjectQuery<ChiroGroep> _chiroGroepen;
+        #endregion
+
+        #region constructors
+        public Cg2ObjectContext() : base("name=Cg2ObjectContext", "Cg2ObjectContext") { }
+        public Cg2ObjectContext(string connectionString) : base(connectionString, "Cg2ObjectContext") { }
+        public Cg2ObjectContext(global::System.Data.EntityClient.EntityConnection connection) : base(connection, "Cg2ObjectContext") { }
+        #endregion
+
+        public global::System.Data.Objects.ObjectQuery<Groep> Groepen
+        {
+            get
+            {
+                if (this._groepen == null)
+                {
+                    this._groepen = base.CreateQuery<Groep>("[Groep]");
+                }
+                return this._groepen;
+            }
+        }
+
+        public global::System.Data.Objects.ObjectQuery<ChiroGroep> ChiroGroepen
+        {
+            get
+            {
+                if (this._chiroGroepen == null)
+                {
+                    this._chiroGroepen = base.CreateQuery<ChiroGroep>("[ChiroGroep]");
+                }
+                return this._chiroGroepen;
+            }
+        }
+
+    }
+}
