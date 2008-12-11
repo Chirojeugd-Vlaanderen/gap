493cb6b3db7322a1c1675a818ed3368bb009cdda
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2008-12-11 10:52:21 +0000

    et voila: een service.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@174 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ poc/EntityFramework/WebServices/GroepenService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Cg2.Core.Domain;
+using Cg2.Util;
+
+namespace WebServices
+{
+    // NOTE: If you change the class name "GroepenService" here, you must also update the reference to "GroepenService" in Web.config.
+    public class GroepenService : IGroepenService
+    {
+        IServiceFactory serviceFactory;
+
+        #region IGroepenService Members
+
+        public Groep Updaten(Groep g, Groep origineel)
+        {
+            serviceFactory = new ClassServiceFactory();
+            IGroepenManager gm = (IGroepenManager)serviceFactory
+                .FindByServiceName("Cg2/Core/Domain/IGroepenManager");
+            return gm.Updaten(g, origineel);
+        }
+
+        #endregion
+    }
+}
---
+++ poc/EntityFramework/WebServices/IGroepenService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Cg2.Core.Domain;
+
+namespace WebServices
+{
+    // NOTE: If you change the interface name "IGroepenService" here, you must also update the reference to "IGroepenService" in Web.config.
+    [ServiceContract]
+    public interface IGroepenService
+    {
+        /// <summary>
+        /// Persisteert een groep in de database
+        /// </summary>
+        /// <param name="g">Te persisteren groep</param>
+        /// <param name="origineel">Het bewaren zal sneller gaan als een
+        /// oorspronkelijk (ongewijzigd) object meegegeven wordt.  Zonder
+        /// gaat het ook; geef dan null mee als origineel.
+        /// </param>
+        /// <returns>De persoon met eventueel gewijzigde informatie</returns>
+        [OperationContract]
+        Groep Updaten(Groep g, Groep origineel);
+    }
+}
