111bd5980d87ec8a2fe5d83cb182ced4d006b2a2
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-02-03 18:07:29 +0000

    IPersonenDao zat in het verkeerde project.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@245 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ poc/LinqToSql/Cg2.Core/DataInterfaces/IPersonenDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cg2.Core.DataInterfaces;
+using Cg2.Core.Domain;
+
+namespace Cg2.Core.DataInterfaces
+{
+    interface IPersonenDao: IDao<Persoon>
+    {
+        Persoon OphalenMetCommunicatie(int id);
+    }
+}
