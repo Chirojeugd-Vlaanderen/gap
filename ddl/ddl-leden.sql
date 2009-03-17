a77252d37dff102c6e94dacce6ad76ff47b5fcf8
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-03-17 16:17:49 +0000

    Ik zal de wijzigingen committen, maar deze ddl's moeten eens opnieuw gegenereerd worden.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@299 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- ddl/ddl-leden.sql
+++ ddl/ddl-leden.sql
@@ -64,3 +64,167 @@ ALTER TABLE grp.GroepsWerkJaar ADD CONSTRAINT FK_GroepsWerkjaar_Groep
 	FOREIGN KEY (groepID) REFERENCES grp.Groep (groepID)
 go
 
+-- wijzigingen achteraf...
+
+ALTER TABLE lid.Lid
+	DROP CONSTRAINT FK_Lid_Persoon
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+EXECUTE sp_rename N'lid.Lid.PersoonID', N'Tmp_GelieerdePersoonID', 'COLUMN' 
+GO
+EXECUTE sp_rename N'lid.Lid.Tmp_GelieerdePersoonID', N'GelieerdePersoonID', 'COLUMN' 
+GO
+ALTER TABLE lid.Lid ADD CONSTRAINT
+	FK_Lid_GelieerdePersoon FOREIGN KEY
+	(
+	GelieerdePersoonID
+	) REFERENCES pers.GelieerdePersoon
+	(
+	GelieerdePersoonID
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+
+ALTER TABLE lid.Lid
+	DROP CONSTRAINT FK_Lid_GelieerdePersoon
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE lid.Lid ADD CONSTRAINT
+	FK_Lid_GelieerdePersoon FOREIGN KEY
+	(
+	LidID
+	) REFERENCES pers.GelieerdePersoon
+	(
+	GelieerdePersoonID
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+ALTER TABLE lid.Lid
+	DROP COLUMN GelieerdePersoonID
+GO
+
+
+-- het was niet juist :-(
+
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE lid.Lid
+	DROP CONSTRAINT FK_Lid_GelieerdePersoon
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE lid.Lid
+	DROP CONSTRAINT FK_Lid_GroepsWerkjaar
+GO
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE lid.Lid
+	DROP CONSTRAINT DF__Lid__LidgeldBeta__056ECC6A
+GO
+ALTER TABLE lid.Lid
+	DROP CONSTRAINT DF__Lid__NonActief__0662F0A3
+GO
+ALTER TABLE lid.Lid
+	DROP CONSTRAINT DF__Lid__Verwijderd__075714DC
+GO
+ALTER TABLE lid.Lid
+	DROP CONSTRAINT DF__Lid__VolgendWerk__084B3915
+GO
+CREATE TABLE lid.Tmp_Lid
+	(
+	EindeInstapPeriode datetime NULL,
+	LidgeldBetaald bit NOT NULL,
+	NonActief bit NOT NULL,
+	Verwijderd bit NOT NULL,
+	VolgendWerkjaar int NOT NULL,
+	LidID int NOT NULL IDENTITY (1, 1),
+	GroepsWerkjaarID int NOT NULL,
+	GelieerdePersoonID int NOT NULL,
+	Guid uniqueidentifier NOT NULL,
+	Versie timestamp NOT NULL
+	)  ON [PRIMARY]
+GO
+ALTER TABLE lid.Tmp_Lid ADD CONSTRAINT
+	DF__Lid__LidgeldBeta__056ECC6A DEFAULT ((0)) FOR LidgeldBetaald
+GO
+ALTER TABLE lid.Tmp_Lid ADD CONSTRAINT
+	DF__Lid__NonActief__0662F0A3 DEFAULT ((0)) FOR NonActief
+GO
+ALTER TABLE lid.Tmp_Lid ADD CONSTRAINT
+	DF__Lid__Verwijderd__075714DC DEFAULT ((0)) FOR Verwijderd
+GO
+ALTER TABLE lid.Tmp_Lid ADD CONSTRAINT
+	DF__Lid__VolgendWerk__084B3915 DEFAULT ((0)) FOR VolgendWerkjaar
+GO
+SET IDENTITY_INSERT lid.Tmp_Lid ON
+GO
+IF EXISTS(SELECT * FROM lid.Lid)
+	 EXEC('INSERT INTO lid.Tmp_Lid (EindeInstapPeriode, LidgeldBetaald, NonActief, Verwijderd, VolgendWerkjaar, LidID, GroepsWerkjaarID)
+		SELECT EindeInstapPeriode, LidgeldBetaald, NonActief, Verwijderd, VolgendWerkjaar, LidID, GroepsWerkjaarID FROM lid.Lid WITH (HOLDLOCK TABLOCKX)')
+GO
+SET IDENTITY_INSERT lid.Tmp_Lid OFF
+GO
+DROP TABLE lid.Lid
+GO
+EXECUTE sp_rename N'lid.Tmp_Lid', N'Lid', 'OBJECT' 
+GO
+ALTER TABLE lid.Lid ADD CONSTRAINT
+	PK_Lid PRIMARY KEY CLUSTERED 
+	(
+	LidID
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+
+GO
+ALTER TABLE lid.Lid ADD CONSTRAINT
+	FK_Lid_GroepsWerkjaar FOREIGN KEY
+	(
+	GroepsWerkjaarID
+	) REFERENCES grp.GroepsWerkJaar
+	(
+	GroepsWerkjaarID
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+ALTER TABLE lid.Lid ADD CONSTRAINT
+	FK_Lid_GelieerdePersoon FOREIGN KEY
+	(
+	GelieerdePersoonID
+	) REFERENCES pers.GelieerdePersoon
+	(
+	GelieerdePersoonID
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+COMMIT
+go
+
+-- unique key 
+CREATE UNIQUE NONCLUSTERED INDEX AK_Lid_GelieerdePersoonID_GroepsWerkJaarID ON lid.Lid
+	(
+	GelieerdePersoonID,
+	GroepsWerkjaarID
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
