6485ec721613fa63b5c144a0f99c5bc2653f0f44
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-04-16 14:27:11 +0000

    Wat geprutst aan AttachObjectGraph zodat die met IBasisEntiteit werkt, en errors met EntityKeys vermeden worden.  Fixes #35.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@325 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/Cg2.Orm/IBasisEntiteit.cs
+++ Solution/Cg2.Orm/IBasisEntiteit.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Objects.DataClasses;
 
 namespace Cg2.Orm
 {
-    public interface IBasisEntiteit
+    public interface IBasisEntiteit: IEntityWithKey
     {
         int ID { get; set; }
         byte[] Versie { get; set; }
