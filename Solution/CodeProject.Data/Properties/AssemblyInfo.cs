60d1e44e01a00e42674d92b263269192996d14b8
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-05-06 12:50:53 +0000

    Check voor CLS-compliante code verwijderd uit project CodeProject.Data
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@344 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/CodeProject.Data/Properties/AssemblyInfo.cs
+++ Solution/CodeProject.Data/Properties/AssemblyInfo.cs
@@ -13,4 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: System.CLSCompliant(true)]
+
+// voorlopig geen CLS-compliante code.
+// [assembly: System.CLSCompliant(true)]
