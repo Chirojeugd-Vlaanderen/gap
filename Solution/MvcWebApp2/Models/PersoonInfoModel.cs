99869ff50720517070c9bbab3313c15713eb0820
BertePe <BertePe@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-05-22 19:36:37 +0000

    ref #58 - PersoonInfoModel toegevoegd
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@385 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ Solution/MvcWebApp2/Models/PersoonInfoModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Capgemini.Adf.ServiceModel;
+using Cg2.Orm;
+using Cg2.ServiceContracts;
+
+namespace MvcWebApp2.Models
+{
+    public class PersoonInfoModel : MasterViewModel
+    {
+        public IList<PersoonInfo> PersoonInfoLijst { get; set; }
+
+        // public PersoonInfo HuidigePersoon { get; set; }
+
+        public PersoonInfoModel() : base() { }
+    }
+}
