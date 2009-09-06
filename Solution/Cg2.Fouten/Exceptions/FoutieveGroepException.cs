fe0ed016375b5794bcc1d72ed2ea4787e1aee975
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-09-06 15:51:16 +0000

    Exception voor 'groep mismatch' (bijv een functie aan een lid toekennen als functie en lid van een verschillende groep zijn.)
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@498 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ Solution/Cg2.Fouten/Exceptions/FoutieveGroepException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Cg2.Fouten.Exceptions
+{
+    /// <summary>
+    /// Exception voor een 'Groep Mismatch', bijv. bij het koppelen
+    /// van een categorie van groep A aan een gelieerd persoon van
+    /// groep B.
+    /// </summary>
+    public class FoutieveGroepException: System.Exception, ISerializable
+    {
+        public FoutieveGroepException(string message) : base(message) { }
+    }
+}
