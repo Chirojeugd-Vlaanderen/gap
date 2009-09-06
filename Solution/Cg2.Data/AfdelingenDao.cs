e771c04d92bb5d7ee8b9fa9672ddd9f045240148
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-09-06 15:50:21 +0000

    DAO voor afdelingen.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@497 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ Solution/Cg2.Data/AfdelingenDao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cg2.Data.Ef;
+using Cg2.Orm;
+using Cg2.Orm.DataInterfaces;
+using System.Data.Objects;
+
+namespace Cg2.Data
+{
+    public class AfdelingenDao: Dao<Afdeling>, IAfdelingenDao
+    {
+        /// <summary>
+        /// Afdeling ophalen op basis van ID.
+        /// </summary>
+        /// <param name="afdelingID">ID van gewenste afdeling</param>
+        /// <returns>Afdeling en gekoppelde groep</returns>
+        public Afdeling Ophalen(int afdelingID)
+        {
+            Afdeling resultaat;
+
+            using (ChiroGroepEntities db = new ChiroGroepEntities())
+            {
+                db.Afdeling.MergeOption = MergeOption.NoTracking;
+
+                resultaat = (
+                    from Afdeling afd
+                    in db.Afdeling.Include("Groep")
+                    where afd.ID == afdelingID
+                    select afd).FirstOrDefault();
+
+                if (resultaat != null)
+                {
+                    // Je zou verwachten dat de Include hierboven
+                    // meteen ook de groep ophaalt, maar dat blijkt
+                    // niet het geval.
+
+                    resultaat.GroepReference.Load();
+                }
+                return resultaat;
+            }
+        }
+    }
+}
