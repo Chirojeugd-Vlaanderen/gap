70c526154a5275d51aba60fe3f626b0b60dc9f64
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-09-10 13:42:23 +0000

    Vergeten de nieuwe functies voor LedenDao toe te voegen aan ILedenDao.  (Dat komt ervan als je geen IoC gebruikt om in de tests DAO's te maken.)
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@516 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/Cg2.Orm/DataInterfaces/ILedenDao.cs
+++ Solution/Cg2.Orm/DataInterfaces/ILedenDao.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Linq.Expressions;
 
 namespace Cg2.Orm.DataInterfaces
 {
@@ -16,6 +17,25 @@ namespace Cg2.Orm.DataInterfaces
         IList<Lid> PaginaOphalen(int groepsWerkJaarID);
 
         IList<Lid> PaginaOphalen(int groepsWerkJaarID, int afdelingsID);
+
+        /// <summary>
+        /// Zoekt lid op op basis van GelieerdePersoonID en GroepsWerkJaarID
+        /// </summary>
+        /// <param name="gelieerdePersoonID">ID van gelieerde persoon</param>
+        /// <param name="groepsWerkJaarID">ID van groepswerkjaar</param>
+        /// <returns>Lidobject indien gevonden, anders null</returns>
+        Lid Ophalen(int gelieerdePersoonID, int groepsWerkJaarID);
+
+        /// <summary>
+        /// Haalt lid met gerelateerde entity's op, op basis van 
+        /// GelieerdePersoonID en GroepsWerkJaarID
+        /// </summary>
+        /// <param name="gelieerdePersoonID">ID van gelieerde persoon</param>
+        /// <param name="groepsWerkJaarID">ID van groepswerkjaar</param>
+        /// <param name="paths">lambda-expressies die de extra op te halen
+        /// informatie definieren</param>
+        /// <returns>Lidobject indien gevonden, anders null</returns>
+        Lid Ophalen(int gelieerdePersoonID, int groepsWerkJaarID, params Expression<Func<Lid, object>>[] paths);
 
         Lid OphalenMetDetails(int lidID);
 
