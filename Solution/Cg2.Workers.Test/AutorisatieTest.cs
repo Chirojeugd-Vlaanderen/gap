5bde643f5d2df26d778de7cbf5f8ca42cb620eb9
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-09-22 07:31:35 +0000

    Test voor ticket #48. Refs #48, #78
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@536 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/Cg2.Workers.Test/AutorisatieTest.cs
+++ Solution/Cg2.Workers.Test/AutorisatieTest.cs
@@ -7,6 +7,7 @@ using Moq;
 using Cg2.Orm.DataInterfaces;
 using Cg2.Orm;
 using Cg2.Fouten.Exceptions;
+using Cg2.Dummies;
 
 namespace Cg2.Workers.Test
 {
@@ -122,6 +123,78 @@ namespace Cg2.Workers.Test
             // Assert
 
             Assert.IsTrue(lijst != null);
+        }
+
+        /// <summary>
+        /// Kijk na of adresgenoten van groepen waarvan je geen GAV
+        /// bent toch niet worden opgehaald.
+        /// </summary>
+        [TestMethod]
+        public void AdresGenotenEnkelEigenGroep()
+        {
+            #region Arrange
+
+            PersonenManager pm = new PersonenManager(null); // geen DAO nodig voor test
+            GelieerdePersonenManager gpm = new GelieerdePersonenManager(null, null
+                , new AutMgrAltijdGav());
+
+            // Stel een situatie op waarbij er 3 personen op hetzelfde
+            // adres wonen, en waarbij 2 van die 3 personen gelieerd
+            // zijn aan jouw groep.
+
+            Groep g1 = new Groep { ID = 1 };
+            Groep g2 = new Groep { ID = 2 };
+            Persoon p1 = new Persoon { ID = 1 };
+            Persoon p2 = new Persoon { ID = 2 };
+            Persoon p3 = new Persoon { ID = 3 };
+            Adres a = new Adres { ID = 1 };
+
+            // koppel de 3 personen aan hetzelfde adres
+
+            pm.AdresToevoegen(p1, a);
+            pm.AdresToevoegen(p2, a);
+            pm.AdresToevoegen(p3, a);
+
+            // p1 en p3 koppelen aan g1, p2 aan g2, en
+            // meteen fake GelieerdePersoonID's geven.
+
+            gpm.PersoonAanGroepKoppelen(p1, g1, 0).ID = 1;
+            gpm.PersoonAanGroepKoppelen(p3, g1, 0).ID = 3;
+            gpm.PersoonAanGroepKoppelen(p2, g2, 0).ID = 2;
+
+
+            // Creeer nu de GelieerdePersonenDaoMock, dia alle huisgenoten van p1 ophaalt.
+
+            var gpDaoMock = new Mock<IGelieerdePersonenDao>();
+            gpDaoMock.Setup(foo => foo.HuisGenotenOphalen(1)).Returns(new List<Persoon> { p1, p2, p3 });
+
+            // en een AutorisatieManagerMock, die zorgt dat gebruiker alvast toegang heeft tot p1.
+
+            var auManMock = new Mock<IAutorisatieManager>();
+            auManMock.Setup(foo => foo.IsGavGelieerdePersoon(1)).Returns(true);
+
+            // Tenslotte een nieuwe gelieerdePersonenManager die we willen
+            // testen.
+
+            GelieerdePersonenManager gpm2 = new GelieerdePersonenManager(
+                gpDaoMock.Object, null, auManMock.Object);
+
+
+            #endregion
+
+            #region Act
+            IList<Persoon> huisgenoten = gpm2.HuisGenotenOphalen(p1.ID);
+            #endregion
+
+            #region Assert
+            // We verwachten enkel p1 en p3.
+
+            var idQuery = (from p in huisgenoten select p.ID);
+
+            Assert.IsTrue(idQuery.Contains(1));
+            Assert.IsTrue(idQuery.Contains(3));
+            Assert.IsFalse(idQuery.Contains(2));
+            #endregion
         }
     }
 }
