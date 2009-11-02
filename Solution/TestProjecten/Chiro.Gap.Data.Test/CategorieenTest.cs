d727f0013a34b2572542dcaa0ffe7a0e7ca87895
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-11-02 14:08:37 +0000

    De unit tests lopen nu ook 2 keer achter elkaar goed :-)
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@612 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/TestProjecten/Chiro.Gap.Data.Test/CategorieenTest.cs
+++ Solution/TestProjecten/Chiro.Gap.Data.Test/CategorieenTest.cs
@@ -12,136 +12,144 @@ using Chiro.Cdf.Ioc;
 
 namespace Chiro.Gap.Data.Test
 {
-	 /// <summary>
-	 /// Tests mbt de categorieen die een gelieerde persoon kan hebben.
-	 /// </summary>
-	 [TestClass]
-	 public class CategorieenTest
-	 {
-		  // De 'TeVerwijderen' zorgt er bij 1->veel enkel voor
-		  // dat de link tussen GelieerdePersoon en Categorie
-		  // verwijderd wordt, en de Categorie zelf niet.
-		  // Om dat laatste te bewerkstellingen, had 
-		  // GelieerdePersoon gedecoreerd moeten zijn met
-		  // [AssociationEndBehavior("PersoonsCategorie", Owned=true)]
-		  // dus TODO apart verwijderen van categorie toelaten!
+	/// <summary>
+	/// Tests mbt de categorieen die een gelieerde persoon kan hebben.
+	/// </summary>
+	[TestClass]
+	public class CategorieenTest
+	{
+		// De 'TeVerwijderen' zorgt er bij 1->veel enkel voor
+		// dat de link tussen GelieerdePersoon en Categorie
+		// verwijderd wordt, en de Categorie zelf niet.
+		// Om dat laatste te bewerkstellingen, had 
+		// GelieerdePersoon gedecoreerd moeten zijn met
+		// [AssociationEndBehavior("PersoonsCategorie", Owned=true)]
+		// dus TODO apart verwijderen van categorie toelaten!
 
-		  ICategorieenDao _catdao = null;
-		  IGelieerdePersonenDao _gpdao = null;
-		  int _catID, _gpID, _gp2ID;
+		ICategorieenDao _catdao = null;
+		IGelieerdePersonenDao _gpdao = null;
+		int _catID, _gpID, _gp2ID;
 
-		  public CategorieenTest(){ }
+		public CategorieenTest() { }
 
-		  /// <summary>
-		  ///Gets or sets the test context which provides
-		  ///information about and functionality for the current test run.
-		  ///</summary>
-		  public TestContext TestContext { get; set; }
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext { get; set; }
 
-		  #region Additional test attributes
-		  //
-		  // You can use the following additional attributes as you write your tests:
-		  //
-		  // Use ClassInitialize to run code before running the first test in the class
-		  // [ClassInitialize()]
-		  // public static void MyClassInitialize(TestContext testContext) { }
-		  //
-		  // Use ClassCleanup to run code after all tests in a class have run
-		  // [ClassCleanup()]
-		  // public static void MyClassCleanup() { }
-		  //
-		  // Use TestInitialize to run code before running each test 
-		  // [TestInitialize()]
-		  // public void MyTestInitialize() { }
-		  //
-		  // Use TestCleanup to run code after each test has run
-		  // [TestCleanup()]
-		  // public void MyTestCleanup() { }
-		  //
-		  [ClassInitialize]
-		  static public void TestInit(TestContext context)
-		  {
-				Chiro.Cdf.Ioc.Factory.ContainerInit();
-		  }
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Use TestInitialize to run code before running each test 
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		[ClassInitialize]
+		static public void TestInit(TestContext context)
+		{
+			Chiro.Cdf.Ioc.Factory.ContainerInit();
+		}
 
-		  [TestInitialize()]
-		  public void MyTestInitialize()
-		  {
-				_catdao = Factory.Maak<ICategorieenDao>();
-				_gpdao = Factory.Maak<IGelieerdePersonenDao>();
-				_catID = Properties.Settings.Default.TestCategorieID;
-				_gpID = Properties.Settings.Default.TestGelieerdePersoonID;
-				_gp2ID = Properties.Settings.Default.TestGelieerdePersoon2ID;
-		  }
+		[TestInitialize()]
+		public void MyTestInitialize()
+		{
+			_catdao = Factory.Maak<ICategorieenDao>();
+			_gpdao = Factory.Maak<IGelieerdePersonenDao>();
+			_catID = Properties.Settings.Default.TestCategorieID;
+			_gpID = Properties.Settings.Default.TestGelieerdePersoonID;
+			_gp2ID = Properties.Settings.Default.TestGelieerdePersoon2ID;
+		}
 
-		  [TestCleanup()]
-		  public void MyTestCleanup()
-		  {
-		  }
+		[TestCleanup()]
+		public void MyTestCleanup()
+		{
+		}
 
-		  #endregion
+		#endregion
 
-         /// <summary>
-         /// Test op het ophalen van een categorie
-         /// </summary>
-          [TestMethod]
-          public void CategorieOphalen()
-          {
-              // act
-              Categorie c = _catdao.Ophalen(_catID, foo=>foo.GelieerdePersoon);
+		/// <summary>
+		/// Test op het ophalen van een categorie
+		/// </summary>
+		[TestMethod]
+		public void CategorieOphalen()
+		{
+			// act
+			Categorie c = _catdao.Ophalen(_catID, foo => foo.GelieerdePersoon);
 
-              // assert
-              Assert.IsTrue(c.ID == _catID);
-          }
-		  
+			// assert
+			Assert.IsTrue(c.ID == _catID);
+		}
 
-		  /// <summary>
-		  /// Test op toevoegen van een gelieerde persoon aan een categorie
-		  /// (many->many via PersoonsCategorie)
-		  /// </summary>
-		  [TestMethod]
-		  public void PersoonToevoegenAanCategorie()
-		  {
-				//arrange
-				GelieerdePersoon gp = _gpdao.Ophalen(_gpID);
-				Categorie cat = _catdao.Ophalen(_catID);
 
-				//add
-				// koppel categorie aan groep
-				cat.GelieerdePersoon.Add(gp);
-				gp.Categorie.Add(cat);
+		/// <summary>
+		/// Test op toevoegen van een gelieerde persoon aan een categorie
+		/// (many->many via PersoonsCategorie)
+		/// </summary>
+		[TestMethod]
+		public void PersoonToevoegenAanCategorie()
+		{
+			//arrange
 
-				// bewaar via dao (we zijn de dao aan het testen)
-				_catdao.Bewaren(cat);
+			// Onderstaande test uitvoeren met GelieerdePersoon2, want
+			// achteraf wordt de categorie opnieuw verwijderd.
 
-				//assert
-				IList<GelieerdePersoon> lijst = _catdao.Ophalen(_catID).GelieerdePersoon.ToList();
+			// GelieerdePersoon1 wordt in andere tests geacht een
+			// categorie te hebben.
 
-				var query = (from item in lijst
-								 where item.ID == _gpID
-								 select item);
+			GelieerdePersoon gp = _gpdao.Ophalen(_gp2ID);
+			Categorie cat = _catdao.Ophalen(_catID);
 
-				Assert.IsTrue(query.Count() == 1);
+			//add
+			// koppel categorie aan groep
+			cat.GelieerdePersoon.Add(gp);
+			gp.Categorie.Add(cat);
 
-				//cleanup
-				cat.GelieerdePersoon.First().TeVerwijderen = true;
-				_catdao.Bewaren(cat);
-		  }
+			// bewaar via dao (we zijn de dao aan het testen)
+			_catdao.Bewaren(cat);
 
-          [TestMethod]
-          public void TestHashCodes()
-          {
-              GelieerdePersoon p1 = new GelieerdePersoon();
-              p1.ID = 10;
-              GelieerdePersoon p2 = new GelieerdePersoon();
-              p2.ID = 10;
-              Categorie c1 = new Categorie();
-              c1.ID = 100;
-              Categorie c2 = new Categorie();
-              c2.ID = 10;
-              Assert.AreEqual(p1.GetHashCode(), p2.GetHashCode());
-              Assert.AreEqual(c1.GetHashCode(), c2.GetHashCode());
-              Assert.AreNotEqual(c1.GetHashCode(), p1.GetHashCode());
-          }
-	 }
+			//assert
+			IList<GelieerdePersoon> lijst = _catdao.Ophalen(_catID).GelieerdePersoon.ToList();
+
+			var query = (from item in lijst
+				     where item.ID == _gpID
+				     select item);
+
+			Assert.IsTrue(query.Count() == 1);
+
+			//cleanup
+
+			cat.GelieerdePersoon.Where(koppeling => koppeling.ID == _gp2ID).First().TeVerwijderen = true;
+			_catdao.Bewaren(cat);
+		}
+
+		[TestMethod]
+		public void TestHashCodes()
+		{
+			GelieerdePersoon p1 = new GelieerdePersoon();
+			p1.ID = 10;
+			GelieerdePersoon p2 = new GelieerdePersoon();
+			p2.ID = 10;
+			Categorie c1 = new Categorie();
+			c1.ID = 100;
+			Categorie c2 = new Categorie();
+			c2.ID = 10;
+			Assert.AreEqual(p1.GetHashCode(), p2.GetHashCode());
+			Assert.AreEqual(c1.GetHashCode(), c2.GetHashCode());
+			Assert.AreNotEqual(c1.GetHashCode(), p1.GetHashCode());
+		}
+	}
 }
