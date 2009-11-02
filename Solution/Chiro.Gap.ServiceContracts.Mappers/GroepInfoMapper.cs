1176a2363aa25dc6302a441216895b29f6cb13ba
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-11-02 15:25:19 +0000

    GroepInfoMapper gebruikt nu automapper.  Refs #87.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@614 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/Chiro.Gap.ServiceContracts.Mappers/GroepInfoMapper.cs
+++ Solution/Chiro.Gap.ServiceContracts.Mappers/GroepInfoMapper.cs
@@ -4,22 +4,55 @@ using System.Linq;
 using System.Text;
 using Chiro.Gap.Orm;
 using Chiro.Gap.ServiceContracts;
+using AutoMapper;
 
 namespace Chiro.Gap.ServiceContracts.Mappers
 {
+    /// <summary>
+    /// Klasse voor het mappen van een Groep naar GroepInfo.
+    /// Gebruikt automapper.  De statische constructor zal automapper configureren.
+    /// </summary>
     public class GroepInfoMapper
     {
-        public static GroepInfo mapGroep(Groep g)
+        /// <summary>
+        /// Configureert automapper zodat de mapfuncties naar behoren werken.
+        /// </summary>
+        static GroepInfoMapper()
         {
-            return new GroepInfo()
-            {
-                ID = g.ID,
-                Groepsnaam = g.Naam,
-                Plaats = (g is ChiroGroep ? (g as ChiroGroep).Plaats : Properties.Resources.NietVanToepassing),
-                StamNummer = g.Code == null ? String.Empty : g.Code.ToUpper()
-            };
+            Mapper.CreateMap<Groep, GroepInfo>()
+                .ForMember(dst => dst.Plaats, opt => opt.MapFrom(
+                    src => src is ChiroGroep ? (src as ChiroGroep).Plaats : Properties.Resources.NietVanToepassing))
+                .ForMember(dst => dst.StamNummer, opt => opt.MapFrom(
+                    src => src.Code == null ? String.Empty : src.Code.ToUpper()));
         }
 
+        /// <summary>
+        /// Mapt een Groep naar GroepInfo
+        /// </summary>
+        /// <param name="src">te mappen groep</param>
+        /// <param name="dst">groepinfo waarnaar te mappen</param>
+        public static void Map(Groep src, GroepInfo dst)
+        {
+            Mapper.Map(src, dst);
+        }
+
+        /// <summary>
+        /// Mapt een Groep naar GroepInfo
+        /// </summary>
+        /// <param name="g">te mappen Groep</param>
+        /// <returns>gemapte GroepInfo</returns>
+        public static GroepInfo mapGroep(Groep g)
+        {
+            GroepInfo result = new GroepInfo();
+            Map(g, result);
+            return result;
+        }
+
+        /// <summary>
+        /// Mapt een rij groepen op een rij GroepInfo-objecten.
+        /// </summary>
+        /// <param name="groepen">rij te mappen groepen</param>
+        /// <returns>rij gemapte GroepInfo</returns>
         public static IList<GroepInfo> mapGroepen(IEnumerable<Groep> groepen)
         {
             IList<GroepInfo> giList = new List<GroepInfo>();
