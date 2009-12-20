cace3b98c86d54bf89f80857d89f01296b9f811c
booneba <booneba@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2009-12-20 00:44:39 +0000

    de tabel CommunicatieType moet de regular expression bevatten voor validatie van input, en een voorbeeld van goede input. Die gegevens werden toegevoegd aan het script.
    Refs #124
    
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@729 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- database/cg2_db_scripts/sql/populate.pers.CommunicatieType.sql
+++ database/cg2_db_scripts/sql/populate.pers.CommunicatieType.sql
@@ -1,12 +1,12 @@
-INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie]) VALUES ('Telefoonnummer', '.*');
+INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie],[Voorbeeld]) VALUES ('Telefoonnummer', '^0[1-9]{1,2}\-[0-9]{2,3}\s?[0-9]{2}\s?[0-9]{2}$|^04[1-9]{2}\-[0-9]{2,3}\s?[0-9]{2}\s?[0-9]{2}$', '03-231 07 95');
 GO
-INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie]) VALUES ('Fax', '.*');
+INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie],[Voorbeeld]) VALUES ('Fax', '^0[1-9]{1,2}\-[0-9]{2,3}\s?[0-9]{2}\s?[0-9]{2}$', '03-232 51 62');
 GO
-INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie]) VALUES ('E-mail', '.*');
+INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie],[Voorbeeld]) VALUES ('E-mail', '^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$', 'iemand@chiro.be');
 GO
-INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie]) VALUES ('Website', '.*');
+INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie],[Voorbeeld]) VALUES ('Website', '.*', 'www.chiro.be');
 GO
-INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie]) VALUES ('MSN', '.*');
+INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie],[Voorbeeld]) VALUES ('MSN', '^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$', 'iemand@chiro.be');
 GO
-INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie]) VALUES ('Jabber (Google)', '.*');
+INSERT INTO [pers].[CommunicatieType] ([Omschrijving],[Validatie],[Voorbeeld]) VALUES ('Jabber (Google)', '.*');
 GO
