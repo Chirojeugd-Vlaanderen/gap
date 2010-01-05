3611fda4b59933ba23f12d9a3b8a468c3fb9be78
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-01-05 13:37:45 +0000

    refs #190. (documentatie)
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@795 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/Chiro.Gap.Fouten/FaultContracts/DataContractFault.cs
+++ Solution/Chiro.Gap.Fouten/FaultContracts/DataContractFault.cs
@@ -6,32 +6,33 @@ using System.Runtime.Serialization;
 
 namespace Chiro.Gap.Fouten.FaultContracts
 {
-    /// <summary>
-    /// Als er een fout optreedt aan de businesskant, zal de service
-    /// een BusinessFault terugsturen.
-    /// </summary>
-    /// <typeparam name="T">Type van de foutcode (meestal een enum)</typeparam>
-    [DataContract]
-    public class DataContractFault<T>
-    {
-        [DataMember]
-        public Dictionary<string, FoutBericht<T>> Berichten { get; set; }
+	/// <summary>
+	/// Een <c>DataContractFault</c> is een probleem met de gegevens in het datacontract, waarbij er
+	/// specifiek gezegd kan worden in welk 'onderdeel' van het datacontract de problemen zitten
+	/// (vandaar de dictionary <c>Berichten</c>, die een 'onderdeel' koppelt met een foutbericht)
+	/// </summary>
+	/// <typeparam name="T">Type van de foutcode (meestal een enum)</typeparam>
+	[DataContract]
+	public class DataContractFault<T>
+	{
+		[DataMember]
+		public Dictionary<string, FoutBericht<T>> Berichten { get; set; }
 
-        public DataContractFault()
-        {
-            Berichten = new Dictionary<string, FoutBericht<T>>();
-        }
+		public DataContractFault()
+		{
+			Berichten = new Dictionary<string, FoutBericht<T>>();
+		}
 
-        /// <summary>
-        /// Voegt een foutbericht toe
-        /// </summary>
-        /// <param name="foutCode">foutcode</param>
-        /// <param name="component">omschrijving van de component waar het fout 
-        /// liep.</param>
-        /// <param name="bericht">foutboodschap.  Mag ook leeg zijn.</param>
-        public void BerichtToevoegen(T foutCode, string component, string bericht)
-        {
-            Berichten.Add(component, new FoutBericht<T> { FoutCode = foutCode, Bericht = bericht });
-        }
-    }
+		/// <summary>
+		/// Voegt een foutbericht toe
+		/// </summary>
+		/// <param name="foutCode">foutcode</param>
+		/// <param name="component">omschrijving van de component waar het fout 
+		/// liep.</param>
+		/// <param name="bericht">foutboodschap.  Mag ook leeg zijn.</param>
+		public void BerichtToevoegen(T foutCode, string component, string bericht)
+		{
+			Berichten.Add(component, new FoutBericht<T> { FoutCode = foutCode, Bericht = bericht });
+		}
+	}
 }
