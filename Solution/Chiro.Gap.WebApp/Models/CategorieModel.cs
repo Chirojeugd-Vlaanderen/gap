1e4d5d6c486f2b757cacef7e55f55b61f84089a7
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-02-11 11:16:57 +0000

    Refs #95.  Tabs voor indentatie (coding guideline 38)
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@893 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/Chiro.Gap.WebApp/Models/CategorieModel.cs
+++ Solution/Chiro.Gap.WebApp/Models/CategorieModel.cs
@@ -9,25 +9,25 @@ using Chiro.Gap.ServiceContracts;
 
 namespace Chiro.Gap.WebApp.Models
 {
-    /// <summary>
-    /// Model gebruikt om iemand een nieuw adres te geven.
-    /// </summary>
-    public class CategorieModel : MasterViewModel 
-    {
-        /// <summary>
-        /// Nieuwe categorieen voor de gegeven gelieerde personen
-        /// </summary>
-        public IEnumerable<Categorie> Categorieen { get; set; }
-        public List<int> GeselecteerdeCategorieen { get; set; }
+	/// <summary>
+	/// Model gebruikt om iemand een nieuw adres te geven.
+	/// </summary>
+	public class CategorieModel : MasterViewModel
+	{
+		/// <summary>
+		/// Nieuwe categorieen voor de gegeven gelieerde personen
+		/// </summary>
+		public IEnumerable<Categorie> Categorieen { get; set; }
+		public List<int> GeselecteerdeCategorieen { get; set; }
 
-        public List<int> GelieerdePersonenIDs { get; set; }
-        public List<string> GelieerdePersonenNaam { get; set; }
+		public List<int> GelieerdePersonenIDs { get; set; }
+		public List<string> GelieerdePersonenNaam { get; set; }
 
-        /// <summary>
-        /// Standaardconstructor - creeert lege
-        /// </summary>
-        public CategorieModel()
-        {
-        }
-    }
+		/// <summary>
+		/// Standaardconstructor - creeert lege
+		/// </summary>
+		public CategorieModel()
+		{
+		}
+	}
 }
