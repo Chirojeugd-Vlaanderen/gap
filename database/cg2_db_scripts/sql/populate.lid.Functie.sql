ddaecef3c2590dcf01434139ac00f92bd8b1d4eb
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-03-01 11:16:30 +0000

    Refs #330.  Het is gemakkelijk als we het ID kennen van de nationaal bepaalde functies.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@911 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- database/cg2_db_scripts/sql/populate.lid.Functie.sql
+++ database/cg2_db_scripts/sql/populate.lid.Functie.sql
@@ -1,8 +1,103 @@
-insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Contactpersoon', 'GG1', 1, 1, 16, 0, 0);
-insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Groepsleiding', 'GG2', 0, 0, 16, 0, 0);
-insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('VB', 'GV1', 0, 0, 18, 0, 0);
-insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Financieel verantwoordelijke', 'FI', 1, 1, 16, 0, 0);
-insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Vertegenwoordiger in jeugdraad', 'JR', 0, 0, 16, 0, 0);
-insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Contactpersoon kookploeg', 'KK', 0, 0, 16, 0, 0);
-insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Proost', 'GP', 0, 0, 16, 0, 0);
+
+
+declare @gg1Code as varchar(5); set @gg1Code='GG1';
+declare @gg2Code as varchar(5); set @gg2Code='GG2';
+declare @gv1Code as varchar(5); set @gv1Code='GV1';
+declare @fiCode as varchar(5); set @fiCode='FI';
+declare @jrCode as varchar(5); set @jrCode='JR';
+declare @kkCode as varchar(5); set @kkCode='KK';
+declare @gpCode as varchar(5); set @gpCode='GP';
+
+
+
+declare @gg1ID as int;
+declare @gg2ID as int;
+declare @gv1ID as int;
+declare @fiID as int;
+declare @jrID as int;
+declare @kkID as int;
+declare @gpID as int;
+
+
+if not exists (select 1 from lid.Functie where code=@gg1Code)
+begin
+	insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Contactpersoon', @gg1Code, 1, 1, 16, 0, 0);
+	set @gg1ID = scope_identity();
+end
+else
+begin
+	set @gg1ID = (select FunctieID from lid.Functie where code=@gg1Code);
+end
+
+if not exists (select 1 from lid.Functie where code=@gg2Code)
+begin
+	insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Groepsleiding', @gg2Code, 0, 0, 16, 0, 0);
+	set @gg2ID = scope_identity();
+end
+else
+begin
+	set @gg2ID = (select FunctieID from lid.Functie where code=@gg2Code);
+end
+
+if not exists (select 1 from lid.Functie where code=@gv1Code)
+begin
+	insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('VB', @gv1Code, 0, 0, 18, 0, 0);
+	set @gv1ID = scope_identity();
+end
+else
+begin
+	set @gv1ID = (select FunctieID from lid.Functie where code=@gv1Code);
+end
+
+if not exists (select 1 from lid.Functie where code=@fiCode)
+begin
+	insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Financieel verantwoordelijke', @fiCode, 1, 1, 16, 0, 0);
+	set @fiID = scope_identity();
+end
+else
+begin
+	set @fiID = (select FunctieID from lid.Functie where code=@fiCode);
+end
+
+if not exists (select 1 from lid.Functie where code=@jrCode)
+begin
+	insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Vertegenwoordiger in jeugdraad', @jrCode, 0, 0, 16, 0, 0);
+	set @jrID = scope_identity();
+end
+else
+begin
+	set @jrID = (select FunctieID from lid.Functie where code=@jrCode);
+end
+
+if not exists (select 1 from lid.Functie where code=@kkCode)
+begin
+	insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Contactpersoon kookploeg', @kkCode, 0, 0, 16, 0, 0);
+	set @kkID = scope_identity();
+end
+else
+begin
+	set @kkID = (select FunctieID from lid.Functie where code=@kkCode);
+end
+
+if not exists (select 1 from lid.Functie where code=@gpCode)
+begin
+	insert into lid.Functie(Naam, Code, MinAantal, MaxAantal, MinLeefTijd, WerkJaarVan, WerkJaarTot) values('Proost', @gpCode, 0, 0, 16, 0, 0);
+	set @gpID = scope_identity();
+end
+else
+begin
+	set @gpID = (select FunctieID from lid.Functie where code=@gpCode);
+end
+
+PRINT '// aan te passen in Gap.Orm.Functie.cs:'
+PRINT ''
+PRINT 'public enum GepredefinieerdeFunctie {';
+PRINT '		Geen = 0,'
+PRINT '		ContactPersoon = ' + CAST(@gg1ID AS VARCHAR(10)) + ',';
+PRINT '		GroepsLeiding = ' + CAST(@gg2ID AS VARCHAR(10)) + ',';
+PRINT '		Vb = ' + CAST(@gv1ID AS VARCHAR(10)) + ',';
+PRINT '		FinancieelVerantwoordelijke = ' + CAST(@fiID AS VARCHAR(10)) + ',';
+PRINT '		JeugdRaad = ' + CAST(@jrID AS VARCHAR(10)) + ',';
+PRINT '		KookPloeg = ' + CAST(@kkID AS VARCHAR(10)) + ',';
+PRINT '		Proost = ' + CAST(@gpID AS VARCHAR(10)) + ' };';
 
