d0900379a7ffc56a2aaef080677bf9222f9a0aea
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-03-05 10:43:56 +0000

    Refs #95.  Tabs voor indentatie.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@925 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/Chiro.Gap.Orm/DataInterfaces/ILedenDao.cs
+++ Solution/Chiro.Gap.Orm/DataInterfaces/ILedenDao.cs
@@ -7,33 +7,35 @@ using Chiro.Cdf.Data;
 
 namespace Chiro.Gap.Orm.DataInterfaces
 {
-    public interface ILedenDao: IDao<Lid>
-    {
-        IList<Lid> AllesOphalen(int groepsWerkJaarID);
+	public interface ILedenDao : IDao<Lid>
+	{
+		IList<Lid> AllesOphalen(int groepsWerkJaarID);
 
-        IList<Lid> PaginaOphalenVolgensAfdeling(int groepsWerkJaarID, int afdelingsID);
+		IList<Lid> PaginaOphalenVolgensAfdeling(int groepsWerkJaarID, int afdelingsID);
 
-        /// <summary>
-        /// Zoekt lid op op basis van GelieerdePersoonID en GroepsWerkJaarID
-        /// </summary>
-        /// <param name="gelieerdePersoonID">ID van gelieerde persoon</param>
-        /// <param name="groepsWerkJaarID">ID van groepswerkjaar</param>
-        /// <returns>Lidobject indien gevonden, anders null</returns>
-        Lid Ophalen(int gelieerdePersoonID, int groepsWerkJaarID);
+		/// <summary>
+		/// Zoekt lid op op basis van GelieerdePersoonID en GroepsWerkJaarID
+		/// </summary>
+		/// <param name="gelieerdePersoonID">ID van gelieerde persoon</param>
+		/// <param name="groepsWerkJaarID">ID van groepswerkjaar</param>
+		/// <returns>Lidobject indien gevonden, anders null</returns>
+		Lid Ophalen(int gelieerdePersoonID, int groepsWerkJaarID);
 
-        /// <summary>
-        /// Haalt lid met gerelateerde entity's op, op basis van 
-        /// GelieerdePersoonID en GroepsWerkJaarID
-        /// </summary>
-        /// <param name="gelieerdePersoonID">ID van gelieerde persoon</param>
-        /// <param name="groepsWerkJaarID">ID van groepswerkjaar</param>
-        /// <param name="paths">lambda-expressies die de extra op te halen
-        /// informatie definieren</param>
-        /// <returns>Lidobject indien gevonden, anders null</returns>
-        Lid Ophalen(int gelieerdePersoonID, int groepsWerkJaarID, params Expression<Func<Lid, object>>[] paths);
 
-        Lid OphalenMetDetails(int lidID);
 
-        //void LidMaken(int gelieerdeID);
-    }
+		/// <summary>
+		/// Haalt lid met gerelateerde entity's op, op basis van 
+		/// GelieerdePersoonID en GroepsWerkJaarID
+		/// </summary>
+		/// <param name="gelieerdePersoonID">ID van gelieerde persoon</param>
+		/// <param name="groepsWerkJaarID">ID van groepswerkjaar</param>
+		/// <param name="paths">lambda-expressies die de extra op te halen
+		/// informatie definieren</param>
+		/// <returns>Lidobject indien gevonden, anders null</returns>
+		Lid Ophalen(int gelieerdePersoonID, int groepsWerkJaarID, params Expression<Func<Lid, object>>[] paths);
+
+		Lid OphalenMetDetails(int lidID);
+
+		//void LidMaken(int gelieerdeID);
+	}
 }
