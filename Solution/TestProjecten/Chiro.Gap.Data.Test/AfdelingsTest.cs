a1d3255a85bb83f27ef977751f7976ce7473e0de
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-03-30 09:32:29 +0000

    Refs #95.  Tabs voor indentatie.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@991 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/TestProjecten/Chiro.Gap.Data.Test/AfdelingsTest.cs
+++ Solution/TestProjecten/Chiro.Gap.Data.Test/AfdelingsTest.cs
@@ -12,250 +12,250 @@ using Chiro.Gap.TestDbInfo;
 
 namespace Chiro.Gap.Data.Test
 {
-    /// <summary>
-    /// Data-Access tests voor afdelingen en alles wat
-    /// daarrond hangt.
-    /// </summary>
-    /// <remarks>Deze tests werken op een testgroep in
-    /// de database.  Het script om de testgroep te maken,
-    /// zit in SVN: 
-    /// https://develop.chiro.be/subversion/cg2/trunk/database/TestData/TestData/TestGroepMaken.sql
-    /// 
-    /// De IDs nodig voor de test zitten in de settings van dit project.
-    /// </remarks>
-    [TestClass]
-    public class AfdelingsTest
-    {
-        public AfdelingsTest()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
+	/// <summary>
+	/// Data-Access tests voor afdelingen en alles wat
+	/// daarrond hangt.
+	/// </summary>
+	/// <remarks>Deze tests werken op een testgroep in
+	/// de database.  Het script om de testgroep te maken,
+	/// zit in SVN: 
+	/// https://develop.chiro.be/subversion/cg2/trunk/database/TestData/TestData/TestGroepMaken.sql
+	/// 
+	/// De IDs nodig voor de test zitten in de settings van dit project.
+	/// </remarks>
+	[TestClass]
+	public class AfdelingsTest
+	{
+		public AfdelingsTest()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
 
-        private TestContext testContextInstance;
+		private TestContext testContextInstance;
 
-        /// <summary>
-        /// Gets or sets the test context which provides
-        /// information about and functionality for the current test run.
-        /// </summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
+		/// <summary>
+		/// Gets or sets the test context which provides
+		/// information about and functionality for the current test run.
+		/// </summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
 
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
-
-        [ClassInitialize]
-        static public void TestsInitialiseren(TestContext context)
-        {
-            // Initialiseer IoC-container.  Niet zeker van of dit
-            // een goeie plaats is...
-            Factory.ContainerInit();
-
-            int gwjID = TestInfo.GROEPSWERKJAARID;
-            int afd3ID = TestInfo.AFDELING3ID;
-
-            // Verwijder mogelijk afdelingsjaar voor afdeling3
-
-            IAfdelingsJarenDao ajDao = Factory.Maak<IAfdelingsJarenDao>();
-            AfdelingsJaar aj = ajDao.Ophalen(gwjID, afd3ID);
-
-            if (aj != null)
-            {
-                aj.TeVerwijderen = true;
-                ajDao.Bewaren(aj);
-            }
+		#region Additional test attributes
+		//
+		// You can use the following additional attributes as you write your tests:
+		//
+		// Use ClassInitialize to run code before running the first test in the class
+		// [ClassInitialize()]
+		// public static void MyClassInitialize(TestContext testContext) { }
+		//
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup() { }
+		//
+		// Use TestInitialize to run code before running each test 
+		// [TestInitialize()]
+		// public void MyTestInitialize() { }
+		//
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup() { }
+		//
+		#endregion
 
 
-        }
+		[ClassInitialize]
+		static public void TestsInitialiseren(TestContext context)
+		{
+			// Initialiseer IoC-container.  Niet zeker van of dit
+			// een goeie plaats is...
+			Factory.ContainerInit();
 
-        /// <summary>
-        /// Opzoeken van een testafdelingsjaar
-        /// </summary>
-        [TestMethod]
-        public void AfdelingsJaarOpzoeken()
-        {
-            #region Arrange
-            int gwjID = TestInfo.GROEPSWERKJAARID;
-            int afdID = TestInfo.AFDELING1ID;
+			int gwjID = TestInfo.GROEPSWERKJAARID;
+			int afd3ID = TestInfo.AFDELING3ID;
 
-            IAfdelingsJarenDao dao = Factory.Maak<IAfdelingsJarenDao>();
-            #endregion
+			// Verwijder mogelijk afdelingsjaar voor afdeling3
 
-            #region Act
-            AfdelingsJaar aj = dao.Ophalen(gwjID, afdID);
-            #endregion
+			IAfdelingsJarenDao ajDao = Factory.Maak<IAfdelingsJarenDao>();
+			AfdelingsJaar aj = ajDao.Ophalen(gwjID, afd3ID);
 
-            #region Assert
-            Assert.IsTrue(aj != null);
-            Assert.IsTrue(aj.GroepsWerkJaar.ID == gwjID);
-            Assert.IsTrue(aj.Afdeling.ID == afdID);
-            #endregion
-        }
+			if (aj != null)
+			{
+				aj.TeVerwijderen = true;
+				ajDao.Bewaren(aj);
+			}
 
-        /// <summary>
-        /// Haalt de afdeling op bepaald door TestAfdelingID,
-        /// en controleert of die gekoppeld is aan groep
-        /// bepaald door TestGroepID
-        /// (zie property's)
-        /// </summary>
-        [TestMethod]
-        public void AfdelingOphalen()
-        {
-            #region Arrange
-            int afdID = TestInfo.AFDELING1ID;
-            int groepID = TestInfo.GROEPID;
 
-            IAfdelingenDao dao = Factory.Maak<IAfdelingenDao>();
-            #endregion
+		}
 
-            #region Act
-            Afdeling afd = dao.Ophalen(afdID);
-            #endregion
+		/// <summary>
+		/// Opzoeken van een testafdelingsjaar
+		/// </summary>
+		[TestMethod]
+		public void AfdelingsJaarOpzoeken()
+		{
+			#region Arrange
+			int gwjID = TestInfo.GROEPSWERKJAARID;
+			int afdID = TestInfo.AFDELING1ID;
 
-            #region Assert
-            Assert.IsTrue(afd != null);
-            Assert.IsTrue(afd.Groep.ID == groepID);
-            #endregion
-        }
+			IAfdelingsJarenDao dao = Factory.Maak<IAfdelingsJarenDao>();
+			#endregion
 
-        /// <summary>
-        /// Creeert een AfdelingsWerkJaar voor de afdeling
-        /// bepaald door TestAfdeling3ID in het groepswerkjaar
-        /// bepaald door TestGroepsWerkJaarID (zie settings)
-        /// </summary>
-        [TestMethod]
-        public void AfdelingsJaarCreeren()
-        {
-            // Arrange
+			#region Act
+			AfdelingsJaar aj = dao.Ophalen(gwjID, afdID);
+			#endregion
 
-            IGroepsWerkJaarDao gwDao = Factory.Maak<IGroepsWerkJaarDao>();
-            IAfdelingenDao aDao = Factory.Maak<IAfdelingenDao>();
-            IAfdelingsJarenDao ajDao = Factory.Maak<IAfdelingsJarenDao>();
-            IDao<OfficieleAfdeling> oaDao = Factory.Maak<IDao<OfficieleAfdeling>>();
+			#region Assert
+			Assert.IsTrue(aj != null);
+			Assert.IsTrue(aj.GroepsWerkJaar.ID == gwjID);
+			Assert.IsTrue(aj.Afdeling.ID == afdID);
+			#endregion
+		}
 
-            GroepsWerkJaarManager wjm = Factory.Maak<GroepsWerkJaarManager>();
+		/// <summary>
+		/// Haalt de afdeling op bepaald door TestAfdelingID,
+		/// en controleert of die gekoppeld is aan groep
+		/// bepaald door TestGroepID
+		/// (zie property's)
+		/// </summary>
+		[TestMethod]
+		public void AfdelingOphalen()
+		{
+			#region Arrange
+			int afdID = TestInfo.AFDELING1ID;
+			int groepID = TestInfo.GROEPID;
 
-            int gwjID = TestInfo.GROEPSWERKJAARID;
-            int afd3ID = TestInfo.AFDELING3ID;
-            int oaID = TestInfo.OFFICIELEAFDELINGID;
+			IAfdelingenDao dao = Factory.Maak<IAfdelingenDao>();
+			#endregion
 
-            int van = TestInfo.AFDELING3VAN;
-            int tot = TestInfo.AFDELING3TOT;
+			#region Act
+			Afdeling afd = dao.Ophalen(afdID);
+			#endregion
 
-            // Voor het gemak haal ik groepswerkjaar en afdeling via
-            // de DAO's op ipv via de workers.
+			#region Assert
+			Assert.IsTrue(afd != null);
+			Assert.IsTrue(afd.Groep.ID == groepID);
+			#endregion
+		}
 
-            GroepsWerkJaar gw = gwDao.Ophalen(gwjID, gwj=>gwj.Groep);
-            Afdeling afd = aDao.Ophalen(afd3ID);
-            OfficieleAfdeling oa = oaDao.Ophalen(oaID);
+		/// <summary>
+		/// Creeert een AfdelingsWerkJaar voor de afdeling
+		/// bepaald door TestAfdeling3ID in het groepswerkjaar
+		/// bepaald door TestGroepsWerkJaarID (zie settings)
+		/// </summary>
+		[TestMethod]
+		public void AfdelingsJaarCreeren()
+		{
+			// Arrange
 
-            // Het afdelingsjaar wordt gemaakt door een worker.
+			IGroepsWerkJaarDao gwDao = Factory.Maak<IGroepsWerkJaarDao>();
+			IAfdelingenDao aDao = Factory.Maak<IAfdelingenDao>();
+			IAfdelingsJarenDao ajDao = Factory.Maak<IAfdelingsJarenDao>();
+			IDao<OfficieleAfdeling> oaDao = Factory.Maak<IDao<OfficieleAfdeling>>();
 
-            AfdelingsJaar aj = wjm.AfdelingsJaarMaken(gw, afd, oa, van, tot);
+			GroepsWerkJaarManager wjm = Factory.Maak<GroepsWerkJaarManager>();
 
-            // Act
+			int gwjID = TestInfo.GROEPSWERKJAARID;
+			int afd3ID = TestInfo.AFDELING3ID;
+			int oaID = TestInfo.OFFICIELEAFDELINGID;
 
-            // Bewaren *MOET* gebeuren via de DAO, want het is de
-            // dao die we testen; we willen niet dat een fout in de
-            // worker de test doet failen.
+			int van = TestInfo.AFDELING3VAN;
+			int tot = TestInfo.AFDELING3TOT;
 
-            ajDao.Bewaren(aj);
+			// Voor het gemak haal ik groepswerkjaar en afdeling via
+			// de DAO's op ipv via de workers.
 
-            // Nu opnieuw ophalen.
+			GroepsWerkJaar gw = gwDao.Ophalen(gwjID, gwj => gwj.Groep);
+			Afdeling afd = aDao.Ophalen(afd3ID);
+			OfficieleAfdeling oa = oaDao.Ophalen(oaID);
 
-            AfdelingsJaar aj2 = ajDao.Ophalen(gwjID, afd3ID);
+			// Het afdelingsjaar wordt gemaakt door een worker.
 
-            // Assert
+			AfdelingsJaar aj = wjm.AfdelingsJaarMaken(gw, afd, oa, van, tot);
 
-            Assert.IsTrue(aj2.ID > 0);
-            Assert.IsTrue(aj2.GroepsWerkJaar.ID == gwjID);
-            Assert.IsTrue(aj2.Afdeling.ID == afd3ID);
-            Assert.IsTrue(aj2.OfficieleAfdeling.ID == oaID);
+			// Act
 
-            // Cleanup is niet meer nodig, want het nieuw gemaakte
-            // afdelingsjaar wordt verwijderd in TestsInitialiseren
-        }
+			// Bewaren *MOET* gebeuren via de DAO, want het is de
+			// dao die we testen; we willen niet dat een fout in de
+			// worker de test doet failen.
 
-        /// <summary>
-        /// Controleert of OphalenMetAfdelingen uit GroepenDao
-        /// slechts 1 afdelingsjaar oplevert voor de testafdeling.
-        /// </summary>
-        [TestMethod]
-        public void OphalenMetAfdelingenAfdelingsJaar()
-        {
-            // Arrange
+			ajDao.Bewaren(aj);
 
-            IGroepenDao dao = Factory.Maak<IGroepenDao>();
+			// Nu opnieuw ophalen.
 
-            // Act
+			AfdelingsJaar aj2 = ajDao.Ophalen(gwjID, afd3ID);
 
-            Groep g = dao.OphalenMetAfdelingen(TestInfo.GROEPSWERKJAARID);
+			// Assert
 
-            // Assert
+			Assert.IsTrue(aj2.ID > 0);
+			Assert.IsTrue(aj2.GroepsWerkJaar.ID == gwjID);
+			Assert.IsTrue(aj2.Afdeling.ID == afd3ID);
+			Assert.IsTrue(aj2.OfficieleAfdeling.ID == oaID);
 
-            var ajQuery = (
-                from afd in g.Afdeling
-                where afd.ID == TestInfo.AFDELING1ID
-                select afd.AfdelingsJaar);
+			// Cleanup is niet meer nodig, want het nieuw gemaakte
+			// afdelingsjaar wordt verwijderd in TestsInitialiseren
+		}
 
-            Assert.IsTrue(ajQuery.Count() == 1);
-        }
+		/// <summary>
+		/// Controleert of OphalenMetAfdelingen uit GroepenDao
+		/// slechts 1 afdelingsjaar oplevert voor de testafdeling.
+		/// </summary>
+		[TestMethod]
+		public void OphalenMetAfdelingenAfdelingsJaar()
+		{
+			// Arrange
 
-        /// <summary>
-        /// Controleert de koppelingen Groep->Afdeling->AfdelingsJaar->GroepsWerkJaar->Groep
-        /// na GroepenDao.OphalenMetAfdelingen.
-        /// </summary>
-        [TestMethod]
-        public void OphalenMetAfdelingenGroepsWerkJaar()
-        {
-            // Arrange
+			IGroepenDao dao = Factory.Maak<IGroepenDao>();
 
-            IGroepenDao dao = Factory.Maak<IGroepenDao>();
+			// Act
 
-            // Act
+			Groep g = dao.OphalenMetAfdelingen(TestInfo.GROEPSWERKJAARID);
 
-            Groep g = dao.OphalenMetAfdelingen(TestInfo.GROEPSWERKJAARID);
+			// Assert
 
-            // Assert
+			var ajQuery = (
+			    from afd in g.Afdeling
+			    where afd.ID == TestInfo.AFDELING1ID
+			    select afd.AfdelingsJaar);
 
-            foreach (Afdeling a in g.Afdeling)
-            {
-                foreach (AfdelingsJaar aj in a.AfdelingsJaar)
-                {
-                    Assert.IsTrue(aj.GroepsWerkJaar.Groep.ID == g.ID);
-                }
-            }
+			Assert.IsTrue(ajQuery.Count() == 1);
+		}
 
-        }
-    }
+		/// <summary>
+		/// Controleert de koppelingen Groep->Afdeling->AfdelingsJaar->GroepsWerkJaar->Groep
+		/// na GroepenDao.OphalenMetAfdelingen.
+		/// </summary>
+		[TestMethod]
+		public void OphalenMetAfdelingenGroepsWerkJaar()
+		{
+			// Arrange
+
+			IGroepenDao dao = Factory.Maak<IGroepenDao>();
+
+			// Act
+
+			Groep g = dao.OphalenMetAfdelingen(TestInfo.GROEPSWERKJAARID);
+
+			// Assert
+
+			foreach (Afdeling a in g.Afdeling)
+			{
+				foreach (AfdelingsJaar aj in a.AfdelingsJaar)
+				{
+					Assert.IsTrue(aj.GroepsWerkJaar.Groep.ID == g.ID);
+				}
+			}
+
+		}
+	}
 }
