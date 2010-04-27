ffd2ba544258a0b93e28aa2ee7e52c2604812c47
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-04-27 08:03:55 +0000

    Refs #100.  Ik ga een C#-projectje maken voor de (eenmalige) initiele import.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1051 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ tools/InitieleImport/Chiro.Pdox.Data/Lezer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+
+namespace Chiro.Pdox.Data
+{
+	/// <summary>
+	/// Klasse om data te lezen uit een e-mailbestand van het oude chirogroepprogramma
+	/// </summary>
+	public class Lezer
+	{
+		private string _connectionString;
+
+		/// <summary>
+		/// Creeert een nieuwe lezer
+		/// </summary>
+		/// <param name="path">pad van de directory met daarin de paradoxtabellen</param>
+		public Lezer(string path)
+		{
+			var builder = new StringBuilder("");
+
+			builder.Append(@"Provider=Microsoft.Jet.OLEDB.4.0;");
+			builder.Append(@"Extended Properties=Paradox 5.x;");
+			builder.Append(String.Format("Data Source={0};", path));
+
+			_connectionString = builder.ToString();
+		}
+	}
+}
