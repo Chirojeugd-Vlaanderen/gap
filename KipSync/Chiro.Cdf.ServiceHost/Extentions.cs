429962680fe55a12dfcaa3d30f19b1ca1d4167af
Haepeto <Haepeto@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-05-31 20:01:54 +0000

    refs #322
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1177 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ KipSync/Chiro.Cdf.ServiceHost/Extentions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chiro.Cdf.ServiceModel
+{
+    public static class CollectionExtentions
+    {
+        public static IEnumerable<U> ConvertAll<T,U>(this IEnumerable<T> collection, Converter<T,U> converter)
+        {
+            return collection.Select(item => converter(item));
+        }
+    }
+
+    public static class ArrayExtentions
+    {
+        public static U[] ConvertAll<T, U>(this T[] array, Converter<T, U> converter)
+        {
+            IEnumerable<T> enumerable = array;
+            return enumerable.ConvertAll(converter).ToArray();
+        }
+    }
+}
---
+++ KipSync/Chiro.Cdf.ServiceHost/ServiceHost.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Messaging;
+
+namespace Chiro.Cdf.ServiceModel
+{
+    /// <summary>
+    /// Generic ServiceHost with extra features 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ServiceHost<T> : ServiceHost
+    {
+        public ServiceHost() 
+            : base(typeof(T))
+        {
+        }
+
+        public ServiceHost(params string[] baseAdresses)
+            : base(typeof(T), Convert(baseAdresses))
+        {
+        }
+
+        public ServiceHost(params Uri[] baseAdresses)
+            : base(typeof(T), baseAdresses)
+        {
+        }
+
+        static Uri[] Convert(string[] baseAddresses)
+        {
+            Converter<string, Uri> convert = (address) => new Uri(address);
+            return baseAddresses.ConvertAll(convert);
+        }
+
+        protected override void OnOpening()
+        {
+            foreach (var endpoint in Description.Endpoints)
+            {
+                endpoint.VerifyQueue();
+            }
+            base.OnOpening();
+        }
+    }
+
+    public static class QueuedServiceHelper
+    {
+        public static void VerifyQueue(this ServiceEndpoint endpoint)
+        {
+            if (endpoint.Binding is NetMsmqBinding)
+            {
+                string queue = GetQueueFromUri(endpoint.Address.Uri);
+                if (MessageQueue.Exists(queue) == false)
+                {
+                    MessageQueue.Create(queue, true);
+                }
+            }
+            
+        }
+
+        private static string GetQueueFromUri(Uri uri)
+        {
+            string path = uri.PathAndQuery;
+            string queue = "." + path.Replace(@"/", @"\").Replace("private", "private$");
+            return queue;
+        }
+    }
+}
