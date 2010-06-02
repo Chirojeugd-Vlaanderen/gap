f498f909c8894647fae89dd5520176f391ca576b
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-06-02 08:08:12 +0000

    Refs #100.  Communicatievormen gaan we niet overnemen uit kipadmin, omdat correcte formattering te moeilijk is in SQL, en omdat we willen vermijden dat groep A de e-mailadressen te zien krijgt die groep B aan een persoon koppelde.
    (Communicatievormen komen wel mee uit de aansluitingsbestanden.)
    
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1180 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- database/dataconversie/GroepsPersonenUitKipadmin.sql
+++ database/dataconversie/GroepsPersonenUitKipadmin.sql
@@ -7,7 +7,7 @@ DECLARE @huidigWj AS INTEGER
 
 SET @huidigWj = (SELECT WerkJaar FROM Kipadmin.Dbo.HuidigWerkJaar)
 
-
+-- Deze SP heeft een dubbelzinnige naam; updatet de groepsinfo als die al bestaat.
 exec @groepID = data.spNieuweGroepUitKipadmin @stamNr
 
 
@@ -251,46 +251,6 @@ PRINT 'Communicatievormen overzetten'
 			WHERE NOT EXISTS (SELECT 1 FROM pers.CommunicatieType cg
 									WHERE cg.Omschrijving = kip.Omschrijving COLLATE SQL_Latin1_General_CP1_CI_AI)
 
-INSERT INTO pers.CommunicatieVorm(GelieerdePersoonID, CommunicatieTypeID, Nummer, Voorkeur)
-	SELECT 	gp.GelieerdePersoonID, 
-			pct.CommunicatieTypeID, 
-			ci.Info,
-			0 as voorkeur  -- Voorlopig nog geen voorkeur.
-		FROM pers.Persoon p JOIN kipadmin.dbo.kipContactInfo ci 
-				ON p.AdNummer = ci.AdNr
-			JOIN pers.GelieerdePersoon gp 
-				ON p.PersoonID = gp.PersoonID
-			JOIN kipadmin.dbo.kipContactType ct 
-				ON ci.ContactTypeID = ct.ContactTypeID
-			JOIN pers.CommunicatieType pct 
-				ON pct.Omschrijving = ct.Omschrijving COLLATE SQL_Latin1_General_CP1_CI_AI
-		WHERE gp.GroepID = @GroepID
-			AND NOT EXISTS (SELECT 1 FROM pers.CommunicatieVorm cv2 JOIN pers.GelieerdePersoon gp2 
-										ON cv2.GelieerdePersoonID = gp2.GelieerdePersoonID
-									JOIN pers.Persoon p2 
-										ON gp2.PersoonID = p2.PersoonID
-									WHERE p2.AdNummer = p.AdNummer
-									AND cv2.CommunicatieTypeID = pct.CommunicatieTypeID
-									AND (cv2.Nummer = ci.Info COLLATE SQL_Latin1_General_CP1_CI_AI
-											OR cv2.CommunicatieTypeID <= 2 
-												AND core.ufnEnkelCijfers(cv2.Nummer) = core.ufnEnkelCijfers(ci.Info)))
-
-
--- update voorkeur op basis van kipadmin
-
-UPDATE cv 
-	SET VoorKeur = CASE ci.VolgNr WHEN 1 THEN 1 ELSE 0 END
-		FROM pers.CommunicatieVorm cv JOIN pers.GelieerdePersoon gp 
-					ON cv.GelieerdePersoonID = gp.GelieerdePersoonID
-				JOIN pers.Persoon p 
-					ON gp.PersoonID = p.PersoonID
-				JOIN kipadmin.dbo.kipContactInfo ci 
-					ON p.AdNummer = ci.AdNr 
-						AND ci.ContactTypeID = cv.CommunicatieTypeID 
-						AND (cv.Nummer = ci.Info COLLATE SQL_Latin1_General_CP1_CI_AI
-											OR cv.CommunicatieTypeID <= 2 
-												AND core.ufnEnkelCijfers(cv.Nummer) = core.ufnEnkelCijfers(ci.Info))
-WHERE gp.GroepID = @groepID
 
 
 
