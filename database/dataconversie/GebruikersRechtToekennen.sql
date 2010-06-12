27362c627820eabf3f1b617a7817bfe8b38e61ac
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-06-12 09:46:05 +0000

    Refs #100.  Bijgewerkte stored procedure om gebruikersrecht toe te kennen.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1198 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- database/dataconversie/GebruikersRechtToekennen.sql
+++ database/dataconversie/GebruikersRechtToekennen.sql
@@ -1,8 +1,10 @@
 CREATE PROCEDURE auth.spGebruikersRechtToekennen @stamNr VARCHAR(10), @login VARCHAR(20) AS
--- Verwijdert alle gelieerde personen uit een groep.
+-- Geeft login gebruikersrecht op gegeven groep
+INSERT INTO auth.Gav(Login)
+SELECT @login WHERE NOT EXISTS (SELECT 1 FROM auth.Gav WHERE Login=@login)
 
-INSERT INTO auth.GebruikersRecht(GavID, GroepID)
-SELECT GavID, GroepID FROM auth.Gav gav, grp.Groep g
+INSERT INTO auth.GebruikersRecht(GavID, GroepID, VervalDatum)
+SELECT GavID, GroepID, DateAdd(year, 1, getDate())  FROM auth.Gav gav, grp.Groep g
 WHERE gav.Login=@login
 AND g.Code=@stamnr
 AND NOT EXISTS (SELECT 1 FROM auth.GebruikersRecht WHERE GavID = gav.GavID AND GroepID = g.GroepID)
