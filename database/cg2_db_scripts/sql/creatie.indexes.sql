f1ba2ea910de0da835a3fc26bc545160d4bf411d
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-08-05 13:24:19 +0000

    Refs #38.  Een aantal nuttige indexen.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1317 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- database/cg2_db_scripts/sql/creatie.indexes.sql
+++ database/cg2_db_scripts/sql/creatie.indexes.sql
@@ -10,6 +10,9 @@ GO
 CREATE UNIQUE INDEX AK_Lid_GelieerdePersoonID_GroepsWerkJaarID ON lid.Lid(GelieerdePersoonID, GroepsWerkJaarID)
 GO
 
+CREATE INDEX IDX_Lid_GroepsWerkJaarID ON lid.LID(GroepsWerkJaarID)
+GO
+
 CREATE UNIQUE INDEX AK_GebruikersRecht_GroepID_GavID ON auth.GebruikersRecht(GroepID, GavID)
 GO
 
@@ -17,6 +20,9 @@ CREATE INDEX IDX_GelieerdePersoon_GroepID ON pers.GelieerdePersoon(GroepID)
 GO
 
 CREATE INDEX IDX_GelieerdePersoon_PersoonID ON pers.GelieerdePersoon(PersoonID)
+GO
+
+CREATE INDEX IDX_GelieerdePersoon_VoorkeursAdresID ON pers.GelieerdePersoon(VoorkeursAdresID)
 GO
 
 CREATE UNIQUE INDEX IDX_GroepsWerkJaar_GroepID_WerkJaar ON grp.GroepsWerkJaar(GroepID, WerkJaar)
@@ -35,8 +41,13 @@ GO
 CREATE UNIQUE INDEX IX_Functie_Naam_GroepID ON lid.Functie(Naam, GroepID)
 GO
 
-CREATE UNIQUE INDEX IDX_PersoonsAdres_PersoonID_AdresID ON pers.PersoonsAdres(PersoonID, AdresID) INCLUDE (AdresTypeID, Versie, PersoonsAdresID)
+CREATE UNIQUE INDEX AK_PersoonsAdres_PersoonID_AdresID ON pers.PersoonsAdres(PersoonID, AdresID) INCLUDE (AdresTypeID, Versie, PersoonsAdresID)
 GO
 
+CREATE INDEX IX_PersoonsAdres_AdresID on pers.PersoonsAdres(AdresID)
+
 CREATE INDEX IDX_CommunicatieVorm_GelieerdePersoonID_CommunicatieTypeID_CommunicatieVormID ON pers.CommunicatieVorm(GelieerdePersoonID, CommunicatieTypeID, CommunicatieVormID) INCLUDE (Nota, Nummer, IsGezinsgebonden, Voorkeur, Versie) 
+GO
+
+CREATE INDEX IDX_Lid_GroepsWerkJaarID ON lid.LID(GroepsWerkJaarID)
 GO
