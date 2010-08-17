69a2f9a72080b2b09d153d3a20118146be535de5
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-08-17 14:06:50 +0000

    Refs #333.  Oeps; verkeerde default constraint in changescript db.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1370 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- database/cg2_db_scripts/changes/AfdJaarGeenAutoVerdeling.sql
+++ database/cg2_db_scripts/changes/AfdJaarGeenAutoVerdeling.sql
@@ -1,4 +1,4 @@
 ALTER TABLE lid.AfdelingsJaar ADD
-	GeenAutoVerdeling bit NOT NULL CONSTRAINT DF_AfdelingsJaar_GeenAutoVerdeling DEFAULT 1
+	GeenAutoVerdeling bit NOT NULL CONSTRAINT DF_AfdelingsJaar_GeenAutoVerdeling DEFAULT 0
 GO
 
