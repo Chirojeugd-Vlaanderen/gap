04b17f6aaba196837ce58bccf684497fd87394a6
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-08-19 13:08:52 +0000

    Refs #511.  using voor transactions ontbrak.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1384 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- Solution/Chiro.Gap.Services/GroepenService.svc.cs
+++ Solution/Chiro.Gap.Services/GroepenService.svc.cs
@@ -7,6 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+#if KIPDORP
+using System.Transactions;
+#endif
 
 using AutoMapper;
 using Chiro.Gap.Domain;
