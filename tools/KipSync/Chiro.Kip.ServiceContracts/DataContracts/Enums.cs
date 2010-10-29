885efe5354984e4ebacc4d06341de0fd2b8f9584
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-10-29 09:51:44 +0000

    Refs #840.  Kaderfuncties in kipsync.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1530 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- tools/KipSync/Chiro.Kip.ServiceContracts/DataContracts/Enums.cs
+++ tools/KipSync/Chiro.Kip.ServiceContracts/DataContracts/Enums.cs
@@ -30,13 +30,50 @@ namespace Chiro.Kip.ServiceContracts.DataContracts
 	[DataContract]
 	public enum FunctieEnum
 	{
-		[EnumMember] ContactPersoon = 168,
-		[EnumMember] GroepsLeiding = 169,
-		[EnumMember] Vb = 170,
-		[EnumMember] FinancieelVerantwoordelijke = 153,
-		[EnumMember] JeugdRaad = 17,
-		[EnumMember] KookPloeg = 185,
-		[EnumMember] Proost = 172
+		[EnumMember]
+		ContactPersoon = 168,
+		[EnumMember]
+		GroepsLeiding = 169,
+		[EnumMember]
+		Vb = 170,
+		[EnumMember]
+		FinancieelVerantwoordelijke = 153,
+		[EnumMember]
+		JeugdRaad = 17,
+		[EnumMember]
+		KookPloeg = 185,
+		[EnumMember]
+		Proost = 172,
+		[EnumMember]
+		GroepsLeidingsBijeenkomsten = 7,
+		[EnumMember]
+		SomVerantwoordelijke = 214,
+		[EnumMember]
+		IkVerantwoordelijke = 10,
+		[EnumMember]
+		RibbelVerantwoordelijke = 3,
+		[EnumMember]
+		SpeelclubVerantwoordelijke = 5,
+		[EnumMember]
+		RakwiVerantwoordelijke = 4,
+		[EnumMember]
+		TitoVerantwoordelijke = 6,
+		[EnumMember]
+		KetiVerantwoordelijke = 2,
+		[EnumMember]
+		AspiVerantwoordelijke = 1,
+		[EnumMember]
+		SomGewesten = 162,
+		[EnumMember]
+		OpvolgingStadsGroepen = 167,
+		[EnumMember]
+		Verbondsraad = 166,
+		[EnumMember]
+		Verbondskern = 156,
+		[EnumMember]
+		StartDagVerantwoordelijker = 160,
+		[EnumMember]
+		SbVerantwoordelijke = 152
 	}
 
 	/// <summary>
