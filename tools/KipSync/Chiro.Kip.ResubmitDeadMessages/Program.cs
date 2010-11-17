c62d420f714ded4f7e853b6faf5616abf0bd4f8f
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2010-11-17 09:07:24 +0000

    Closes #865.  Tooltje om berichten te recupereren uit dead message queue.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1549 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ tools/KipSync/Chiro.Kip.ResubmitDeadMessages/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace Chiro.Kip.ResubmitDeadMessages
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("<ENTER> to start...");
+			Console.ReadLine();
+
+			using (var serviceHost = new ServiceHost(typeof(DlqSyncPersoonService)))
+			{
+				serviceHost.Open();
+
+				Console.WriteLine("The dead letter service is ready.");
+				Console.WriteLine("Press <ENTER> to terminate service.");
+
+				Console.ReadLine();
+
+				serviceHost.Close();
+			}
+		}
+	}
+}
