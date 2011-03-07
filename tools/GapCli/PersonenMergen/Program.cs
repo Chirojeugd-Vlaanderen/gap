c6c832cf52a0785a4640c34995ed8051700497de
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2011-03-07 13:05:47 +0000

    Refs #813.  Communicatie kipadmin -> gapupdateservice via command line tools.  Ik ga er niet aan beginnen om vanuit delphi mvc-berichten in een message queue te plaatsen.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1655 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ tools/GapCli/PersonenMergen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using PersonenMergen.Properties;
+
+namespace PersonenMergen
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Count() != 2)
+			{
+				Console.WriteLine(Resources.Gebruik, Environment.CommandLine);
+				Console.ReadLine();
+			}
+			else
+			{
+				using (var svc = new GapUpdateService.UpdateServiceClient())
+				{
+					svc.AdNummerVervangen(int.Parse(args[0]), int.Parse(args[1]));
+				}
+			}
+		}
+	}
+}
