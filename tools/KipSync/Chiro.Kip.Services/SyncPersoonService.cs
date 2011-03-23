b9690ce90fcccf5e55b82489d2eb8105cb3159bb
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2011-03-23 15:45:49 +0000

    Closes #929.  Voor adressen zonder bus moet het natuurlijk ook blijven werken.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1685 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- tools/KipSync/Chiro.Kip.Services/SyncPersoonService.cs
+++ tools/KipSync/Chiro.Kip.Services/SyncPersoonService.cs
@@ -169,7 +169,7 @@ namespace Chiro.Kip.Services
 				// Als dat entities op sql server zijn, dan gebeurt het zoeken sowieso 
 				// hoofdletterongevoelig.
 
-				string huisNr = adres.HuisNr == null ? null : String.Format(Properties.Resources.NrEnBus, adres.HuisNr, adres.Bus);
+				string huisNr = adres.HuisNr == null ? null : String.IsNullOrEmpty(adres.Bus) ? adres.HuisNr.ToString() : String.Format(Properties.Resources.NrEnBus, adres.HuisNr, adres.Bus);
 				string postNr = adres.PostNr.ToString();
 
 				var adresInDb = (from adr in db.AdresSet.Include("kipWoont.kipPersoon").Include("kipWoont.kipAdresType")
