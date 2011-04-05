96c3a4d17a568dd32c6b74769052e782bae2dfd6
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2011-04-05 09:57:39 +0000

    Refs #281. Tabellen voor bivakplaats en uitstap.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1709 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ database/cg2_db_scripts/changes/1.1/bivak.sql
@@ -0,0 +1,38 @@
+use gap
+go
+
+create schema biv;
+go
+
+create table biv.Plaats
+(
+	PlaatsID int not null identity(1,1),
+	Naam varchar(80) not null,
+	AdresID int not null,
+	GelieerdePersoonID int not null,	-- contactpersoon
+	Versie timestamp
+);
+
+alter table biv.Plaats add constraint PK_Plaats primary key(PlaatsID);
+alter table biv.Plaats add constraint FK_Plaats_Adres foreign key(AdresID) references adr.Adres(AdresID);
+alter table biv.Plaats add constraint FK_Plaats_GelieerdePersoon_Contact foreign key (GelieerdePersoonID) references pers.GelieerdePersoon(GelieerdePersoonID);
+
+create table biv.Uitstap
+(
+	UitstapID int not null identity(1,1),
+	Naam varchar(120) not null,
+	DatumVan datetime not null,
+	DatumTot datetime not null,
+	Opmerkingen text,
+	PlaatsID int,
+	GroepsWerkJaarID int
+)
+
+alter table biv.Uitstap add constraint PK_Uitstap primary key(UitstapID);
+alter table biv.Uitstap add constraint FK_Uitstap_Plaats foreign key(PlaatsID) references biv.Plaats(PlaatsID);
+alter table biv.Uitstap add constraint FK_Uitstap_GroepsWerkJaar foreign key(GroepsWerkJaarID) references grp.GroepsWerkJaar(GroepsWerkJaarID);
+
+grant select,insert,update,delete on biv.Plaats to GapRole;
+grant select,insert,update,delete on biv.Uitstap to GapRole;
+
+
