3e8b5bd9148184416dd5d3a11a72f4aacaa069f5
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2011-06-17 08:47:55 +0000

    Refs #1009: verwijderen van onduidelijkheden uit de bivakoverzichttabel.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1813 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ tools/KipSync/KipAdminDB/FixBivakOverzicht.sql
@@ -0,0 +1,27 @@
+-- Een paar fixes in de overzichtstabel voor het bivak,
+-- om verwarring bij het secretariaat te vermijden.
+
+
+-- Verwijder afdelingsbivakken zonder adres uit de overzichtstabel
+
+update biv.BivakOverzicht
+set s_aangifteID=null, s_begin=null, s_eind=null, s_naam=null, s_straat=null, s_postnr=null, s_gemeente=null, s_land=null,s_provinci=null, s_tel=null, s_apart='N'
+where werkjaar=2010 and s_begin is not null
+and s_postnr is null
+
+-- Verwijder 'gewone' bivakken zonder adres uit overzichtstabel
+
+update biv.BivakOverzicht
+set b_aangifteID=null, veran_adnr = null
+where werkjaar=2010 and veran_adnr is not null
+and b_postnr is null
+
+-- Buitenlands bivak en 'afdelingsbivak' expliciet aanvinken, indien ze bestaan
+
+update biv.BivakOverzicht
+set u_buiten='J'
+where werkjaar=2010 and u_begin is not null and (u_buiten is null or u_buiten<>'J')
+
+update biv.BivakOverzicht
+set s_apart='J'
+where werkjaar=2010 and s_begin is not null and (s_apart is null or s_apart<>'J')
