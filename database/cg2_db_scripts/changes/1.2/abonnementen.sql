b6cefd54d97e2b46b060eeb7101a1fad29ff28c1
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2011-07-14 14:27:26 +0000

    Refs #557.  Tabellen voor abonnementen.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1850 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ database/cg2_db_scripts/changes/1.2/abonnementen.sql
@@ -0,0 +1,31 @@
+CREATE SCHEMA abo
+GO
+
+CREATE TABLE abo.Publicatie(
+	PublicatieID INT NOT NULL IDENTITY(1,1),
+	Naam VARCHAR(80) NOT NULL,
+	Versie TIMESTAMP NULL,
+	Actief BIT NOT NULL DEFAULT 1,
+	PRIMARY KEY(PublicatieID))
+
+CREATE TABLE abo.Abonnement(
+	AbonnementID INT NOT NULL IDENTITY(1,1),
+	GelieerdePersoonID INT NOT NULL,
+	PublicatieID INT NOT NULL,
+	GroepsWerkJaarID INT NOT NULL,
+	AanvraagDatum DATETIME NOT NULL DEFAULT getdate(),
+	Versie TIMESTAMP NULL,
+	PRIMARY KEY(AbonnementID))
+	
+ALTER TABLE abo.Abonnement 
+ADD CONSTRAINT FK_Abonnement_GelieerdePersoon FOREIGN KEY(GelieerdePersoonID) REFERENCES pers.GelieerdePersoon;
+
+ALTER TABLE abo.Abonnement 
+ADD CONSTRAINT FK_Abonnement_Publicatie FOREIGN KEY(PublicatieID) REFERENCES abo.Publicatie;
+
+ALTER TABLE abo.Abonnement 
+ADD CONSTRAINT FK_Abonnement_GroepsWerkJaar FOREIGN KEY(GroepsWerkJaarID) REFERENCES grp.GroepsWerkJaar;
+
+CREATE INDEX IDX_Abonnement_GelieerdePersoon ON abo.Abonnement(GelieerdePersoonID);
+CREATE INDEX IDX_Abonnement_GroepsWerkJaar_Publicatie ON abo.Abonnement(GroepsWerkJaarID, PublicatieID);
+
