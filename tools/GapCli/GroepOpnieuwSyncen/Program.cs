d321eb980543374b53429a2dc71a7d9bfa37827b
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2011-08-23 18:42:14 +0000

    Closes #1108.  Fijn command line tooltje dat het bericht 'groepopnieuwsyncen' naar de gapupdate queueu stuurt.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1906 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ tools/GapCli/GroepOpnieuwSyncen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GroepOpnieuwSyncen.Properties;
+
+namespace GroepOpnieuwSyncen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() != 1)
+            {
+                Console.WriteLine(Resources.Gebruik, Environment.CommandLine);
+                Console.ReadLine();
+            }
+            else
+            {
+                using (var svc = new GapUpdateService.UpdateServiceClient())
+                {
+                    svc.OpnieuwSyncen(args[0]);
+                }
+            }
+        }
+    }
+}
