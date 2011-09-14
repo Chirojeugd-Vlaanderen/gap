a7b45979b4bcf1d7f0ea3ecc7d897f370542b3c5
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2011-09-14 08:31:41 +0000

    Refs #1125.  Command line tooltje voor hersyncen abonnementen van een groep.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1929 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ tools/GapCli/AbonnementenOpnieuwSyncen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AbonnementenOpnieuwSyncen.Properties;
+
+namespace AbonnementenOpnieuwSyncen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() != 1)
+            {
+                Console.WriteLine(Resources.Gebruik, Environment.CommandLine);
+                Console.ReadLine();
+            }
+            else
+            {
+                using (var svc = new UpdateServiceReference.UpdateServiceClient())
+                {
+                    svc.AbonnementenOpnieuwSyncen(args[0]);
+                }
+            }
+        }
+    }
+}
