5f3025a3b74fe019d6229b8424cd0d9f14969907
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2011-10-16 20:25:55 +0000

    Refs #1159.  Mock voor de AdService.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@1966 99b415e2-69d1-4072-8d93-0ed3a15484ab

---
+++ tools/LoginService/Chiro.Ad.ServiceContracts/AdServiceMock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chiro.Ad.ServiceContracts
+{
+    /// <summary>
+    /// Dummy-implementatie van de AdService, voor gebruik bij testen.
+    /// </summary>
+    public class AdServiceMock: IAdService
+    {
+        public string GapLoginAanvragen(int adNr, string voornaam, string familienaam, string mailadres)
+        {
+            return String.Format("ONGELDIG-{0}", adNr);
+        }
+    }
+}
