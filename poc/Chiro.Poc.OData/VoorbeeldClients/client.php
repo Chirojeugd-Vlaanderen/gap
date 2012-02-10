1c1512fd5861bab911ef720e5622b92bbf4353c0
vervljo <vervljo@99b415e2-69d1-4072-8d93-0ed3a15484ab>
2012-02-10 11:15:37 +0000

    Refs #845.  PHP-client aangepast zodat basic authentication gebruikt kan worden.
    git-svn-id: https://develop.chiro.be/subversion/cg2/trunk@2070 99b415e2-69d1-4072-8d93-0ed3a15484ab

--- poc/Chiro.Poc.OData/VoorbeeldClients/client.php
+++ poc/Chiro.Poc.OData/VoorbeeldClients/client.php
@@ -9,14 +9,29 @@
 // wijzig onderstaande, zodat het wijst naar een instantie van de service.
 DEFINE('SERVICE_URL', 'http://lap-jve2.chiro.lokaal/ODataPoc/DataService.svc');
 
+// gebruikersnaam en wachtwoord voor basic authenticatie.  
+// Gebruik de lege string als USER als er geen authenticatie nodig is.
+DEFINE('USER','apitest');
+DEFINE('PASS','PubliekGeheim.');
+
 
 // get_data - haalt JSON-data op van de service, en parst het
 // resultaat als php-object.
 function get_data($url)
 {
+	$header="accept: application/json\r\n";
+	if (USER != '')
+	{
+		$header .= 'Authorization: Basic '.base64_encode(USER.':'.PASS);
+	}
+
 	$context = stream_context_create(
-		array('http'=>array('header'=>'accept: application/json')));
-	return json_decode(file_get_contents($url, false, $context))->d;
+		array('http'=>array(
+			'header'=>"accept: application/json\r\n".
+			          'Authorization: Basic '.base64_encode(USER.':'.PASS))));
+
+	$result=file_get_contents(SERVICE_URL."\\$url", false, $context);
+	return json_decode($result)->d;
 }
 
 ?>
@@ -30,9 +45,9 @@ function get_data($url)
 	<table>
 
 <?php
-$leden = get_data(SERVICE_URL."/Leden");
+$leden = get_data("Leden");
 
-foreach ($leden as $lid)
+foreach ($leden->results as $lid)
 {
 ?> 
 	<tr>
