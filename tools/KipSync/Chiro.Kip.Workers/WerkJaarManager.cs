3a6646f7446af9e0fd71873429f8efb9374bf23c
vervljo <johan.vervloet@chiro.be>
2012-06-04 10:28:56 +0200

    Closes #1282.  Werkjaar bivakaangifte wordt bepaald door datum van het bivak.

---
+++ tools/KipSync/Chiro.Kip.Workers/WerkJaarManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chiro.Kip.Workers
+{
+    public class WerkJaarManager
+    {
+        /// <summary>
+        /// Bepaalt het werkjaar dat gekoppeld is aan een datum.  De maand waarop het nieuwe werkjaar begint,
+        /// zit in de settings.
+        /// </summary>
+        /// <param name="datum">Datum waarvan werkjaar te bepalen is</param>
+        /// <returns>Beginjaar van het werkjaar.  Bijv. 2011 voor 2011-2012</returns>
+        public int DatumNaarWerkJaar(DateTime datum)
+        {
+            return datum.Month >= Properties.Settings.Default.WerkJaarStartMaand ? datum.Year : datum.Year - 1;
+        }
+    }
+}
