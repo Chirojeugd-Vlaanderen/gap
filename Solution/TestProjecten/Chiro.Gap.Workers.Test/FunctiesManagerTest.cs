666a7b1104c8be8b248f2ab9e9db2d0664a7cc84
Tom Haepers <tom.haepers@chiro.be>
2012-06-09 21:02:38 +0200

    Twee unit test projecten samen gevoegd

--- Solution/TestProjecten/Chiro.Gap.Workers.Test/FunctiesManagerTest.cs
+++ Solution/TestProjecten/Chiro.Gap.Workers.Test/FunctiesManagerTest.cs
@@ -1,19 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Diagnostics;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Chiro.Cdf.Ioc;
-using Chiro.Gap.Domain;
-using Chiro.Gap.Dummies;
-using Chiro.Gap.Orm;
+using Chiro.Gap.WorkerInterfaces;
 using Chiro.Gap.Workers.Exceptions;
 
-namespace Chiro.Gap.Workers.Test
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Moq;
+
+using Chiro.Cdf.Ioc;
+using Chiro.Gap.Dummies;
+using Chiro.Gap.Orm;
+using Chiro.Gap.Orm.DataInterfaces;
+
+namespace Chiro.Gap.Workers.Test.CustomIoc
 {
-
-
 	/// <summary>
 	/// Dit is een testclass voor Unit Tests van FunctiesManagerTest,
 	/// to contain all FunctiesManagerTest Unit Tests
@@ -21,7 +21,6 @@ namespace Chiro.Gap.Workers.Test
 	[TestClass]
 	public class FunctiesManagerTest
 	{
-
 		#region Additional test attributes
 		// 
 		//You can use the following additional attributes as you write your tests:
@@ -33,356 +32,207 @@ namespace Chiro.Gap.Workers.Test
 			Factory.ContainerInit();
 		}
 		//
-		//Use ClassCleanup to run code after all tests in a class have run
-		//[ClassCleanup()]
-		//public static void MyClassCleanup()
-		//{
-		//}
+		// Use ClassCleanup to run code after all tests in a class have run
+		// [ClassCleanup()]
+		// public static void MyClassCleanup()
+		// { 
+		// }
 		//
-		//Use TestInitialize to run code before running each test
-		//[TestInitialize]
-		//public void MyTestInitialize()
-		//{
-		//}
+		// Use TestInitialize to run code before running each test
+		// [TestInitialize]
+		// public void MyTestInitialize()
+		// {
+		// }
 		//
-		//Use TestCleanup to run code after each test has run
-		//[TestCleanup()]
-		//public void MyTestCleanup()
-		//{
-		//}
+		// Use TestCleanup to run code after each test has run
+		// [TestCleanup()]
+		// public void MyTestCleanup()
+		// {
+		// }
 		//
 		#endregion
 
 		/// <summary>
-		/// Opsporen functie met te veel aantal members
+		/// Controleert of de nationaal bepaalde functies gecachet worden, door te tellen hoe dikwijls
+		/// de DAO opgevraagd wordt.
 		/// </summary>
 		[TestMethod]
-		public void TweeKeerUniekeFunctieToekennenTestVerschillendLid()
+		public void NationaalBepaaldeFunctiesOphalenTest()
 		{
-			// Arrange
+			#region Arrange
 
-			var testData = new DummyData();
+			// Deze DAO's nog eens expliciet registreren, om te vermijden dat wijzigingen in
+			// andere tests een vertekend beeld opleveren.
 
-			var fm = Factory.Maak<FunctiesManager>();
-			var gm = Factory.Maak<GroepenManager>();
+			Factory.InstantieRegistreren<IAutorisatieManager>(new AutMgrAltijdGav());
+			Factory.InstantieRegistreren<ILedenDao>(new DummyLedenDao());
 
-			var f = gm.FunctieToevoegen(
-				testData.DummyGroep,
-				testData.NieuweFunctieNaam,
-				testData.NieuweFunctieCode,
-				1, 0,
-				LidType.Alles);
+			// Mock voor IFunctieDao, die een lege lijst geeft als de nationaal bepaalde functies
+			// opgevraagd worden.  We willen gewoon tellen hoe dikwijls deze opgevraagd wordt.
+			var funDaoMock = new Mock<IFunctiesDao>();
+			funDaoMock.Setup(dao => dao.NationaalBepaaldeFunctiesOphalen()).Returns(new List<Functie>());
+			Factory.InstantieRegistreren(funDaoMock.Object);
 
-			IEnumerable<Functie> functies = new Functie[] { f };
+			var target = Factory.Maak<FunctiesManager>();
 
-			// Act
+			#endregion
 
-			fm.Toekennen(testData.LeiderJos, functies);
-			fm.Toekennen(testData.LidYvonne, functies);
+			#region Act
+			// tweemaal opvragen
+			var resultaat = target.NationaalBepaaldeFunctiesOphalen();
+			resultaat = target.NationaalBepaaldeFunctiesOphalen();
+			#endregion
 
-			// Assert
+			#region Assert
 
-			var problemen = fm.AantallenControleren(testData.HuidigGwj, functies);
-			Assert.AreNotEqual(problemen.Count(), 0);
+			funDaoMock.Verify(dao => dao.NationaalBepaaldeFunctiesOphalen(), Times.AtMost(1), "Nationale functies waren niet gecachet.");
+
+			#endregion
 		}
 
 		/// <summary>
-		/// Als een functie maar 1 keer mag voorkomen, maar ze wordt 2 keer toegekend aan dezelfde
-		/// persoon, dan moet dat zonder problemen kunnen.  
+		/// Test voor ticket #890 
 		/// </summary>
 		[TestMethod]
-		public void TweeKeerUniekeFunctieToekennenTestZelfdeLid()
+		public void FunctieOphalenTest()
 		{
 			// Arrange
 
-			var testData = new DummyData();
+			var auMgrMock = new Mock<IAutorisatieManager>();
+			var funDaoMock = new Mock<IFunctiesDao>();
 
-			FunctiesManager fm = Factory.Maak<FunctiesManager>();
-			GroepenManager gm = Factory.Maak<GroepenManager>();
+			auMgrMock.Setup(mgr => mgr.IsGavCategorie(It.IsAny<int>())).Returns(false);
+			auMgrMock.Setup(mgr => mgr.IsGavFunctie(It.IsAny<int>())).Returns(true);
 
-			Functie f = gm.FunctieToevoegen(
-				testData.DummyGroep,
-				testData.NieuweFunctieNaam,
-				testData.NieuweFunctieCode,
-				1, 0,
-				LidType.Alles);
+			funDaoMock.Setup(mgr => mgr.Ophalen(It.IsAny<int>())).Returns(new Functie());
 
-			IEnumerable<Functie> functies = new Functie[] { f };
+			Factory.InstantieRegistreren<IAutorisatieManager>(auMgrMock.Object);
+			Factory.InstantieRegistreren<IFunctiesDao>(funDaoMock.Object);
 
-			// Act
+			var funMgr = Factory.Maak<FunctiesManager>();
 
-			fm.Toekennen(testData.LeiderJos, functies);
-			fm.Toekennen(testData.LeiderJos, functies);
+			// act
 
-			// Assert
+			var resultaat = funMgr.Ophalen(100, false); // haal functie op zonder iets extra
 
-			var problemen = fm.AantallenControleren(testData.HuidigGwj, functies);
-			Assert.AreEqual(problemen.Count(), 0);
+			// assert
+
+			// Aangezien ik de autorisatiemanager gemockt heb, zodat je rechten krijgt op iedere
+			// functie, moet er een functie opgehaald zijn.
+
+			Assert.IsNotNull(resultaat);
 		}
 
 		/// <summary>
-		/// Het toekennen van een functie die niet geldig is in het huidige werkJaar, moet
-		/// een exception opleveren
+		/// Controleert of een ongebruikte functie probleemloos verwijderd kan worden
 		/// </summary>
-		[ExpectedException(typeof(FoutNummerException))]
 		[TestMethod]
-		public void ToekennenFunctieOngeldigWerkJaar()
+		public void OngebruikteFunctieVerwijderenTest()
 		{
-			// Arrange
+			// arrange
 
 			var testData = new DummyData();
 
-			FunctiesManager fm = Factory.Maak<FunctiesManager>();
-			GroepenManager gm = Factory.Maak<GroepenManager>();
+			var veelGebruiktMock = new Mock<IVeelGebruikt>();
+			veelGebruiktMock.Setup(vgb => vgb.GroepsWerkJaarOphalen(testData.DummyGroep.ID)).Returns(testData.HuidigGwj);
+			Factory.InstantieRegistreren<IVeelGebruikt>(veelGebruiktMock.Object);
 
-			Functie f = gm.FunctieToevoegen(
-				testData.DummyGroep,
-				testData.NieuweFunctieNaam,
-				testData.NieuweFunctieCode,
-				1, 0,
-				LidType.Alles);
+			var mgr = Factory.Maak<FunctiesManager>();
 
-		    f.WerkJaarTot = testData.HuidigGwj.WerkJaar - 1; // vervallen functie
+			// act
 
-			IEnumerable<Functie> functies = new Functie[] { f };
+			var result = mgr.Verwijderen(testData.OngebruikteFunctie, false);
 
-			// Act
+			// assert
 
-			fm.Toekennen(testData.LeiderJos, functies);
+			Assert.IsNull(result);
+		}
 
-			// Assert
+		/// <summary>
+		/// probeert een functie die dit jaar in gebruik is te verwijderen.  We verwachten een exception.
+		/// </summary>
+		[TestMethod]
+		[ExpectedException(typeof(BlokkerendeObjectenException<Lid>))]
+		public void FunctieDitJaarInGebruikVerwijderenTest()
+		{
+			// arrange
+
+			var testData = new DummyData();
+
+			var veelGebruiktMock = new Mock<IVeelGebruikt>();
+			veelGebruiktMock.Setup(vgb => vgb.GroepsWerkJaarOphalen(testData.DummyGroep.ID)).Returns(testData.HuidigGwj);
+			Factory.InstantieRegistreren<IVeelGebruikt>(veelGebruiktMock.Object);
+
+			var mgr = Factory.Maak<FunctiesManager>();
+
+			// act
+
+			var result = mgr.Verwijderen(testData.UniekeFunctie, false);
+
+			// assert
+
+			// Als we hier toekomen zonder exception, dan ging er iets mis.
 			Assert.IsTrue(false);
 		}
 
 		/// <summary>
-		/// Functies voor leiding mogen niet aan een kind toegewezen worden.
+		/// probeert een functie die enkel dit jaar in gebruik is, geforceerd te verwijderen. 
+		/// We verwachten dat ze definitief weg is.
 		/// </summary>
-		[ExpectedException(typeof(InvalidOperationException))]
 		[TestMethod]
-		public void ToekennenLidFunctieAanLeiding()
+		public void FunctieEnkelDitJaarInGebruikGeforceerdVerwijderenTest()
 		{
-			// Arrange
+			// arrange
 
 			var testData = new DummyData();
 
-			FunctiesManager fm = Factory.Maak<FunctiesManager>();
-			GroepenManager gm = Factory.Maak<GroepenManager>();
+			var veelGebruiktMock = new Mock<IVeelGebruikt>();
+			veelGebruiktMock.Setup(vgb => vgb.GroepsWerkJaarOphalen(testData.DummyGroep.ID)).Returns(testData.HuidigGwj);
+			Factory.InstantieRegistreren<IVeelGebruikt>(veelGebruiktMock.Object);
 
-			Functie f = gm.FunctieToevoegen(
-				testData.DummyGroep,
-				testData.NieuweFunctieNaam,
-				testData.NieuweFunctieCode,
-				1, 0,
-				LidType.Kind);
+			var mgr = Factory.Maak<FunctiesManager>();
 
-			IEnumerable<Functie> functies = new Functie[] { f };
+			// act
 
-			// Act
-			fm.Toekennen(testData.LeiderJos, functies);
+			var result = mgr.Verwijderen(testData.UniekeFunctie, true);
 
-			// Assert
-			Assert.IsTrue(false);
+			// assert
+
+			Assert.IsNull(result);
 		}
 
 		/// <summary>
-		/// Verplichte functie die niet toegekend wordt
+		/// probeert een functie die zowel dit jaar als vorig jaar gebruikt is, 
+		/// geforceerd te verwijderen.  We verwachten dat het 'werkJaar tot'  wordt
+		/// ingevuld.
 		/// </summary>
 		[TestMethod]
-		public void NietToegekendeVerplichteFunctie()
+		public void FunctieLangerInGebruikGeforceerdVerwijderenTest()
 		{
-			// Arrange
+			// arrange
 
 			var testData = new DummyData();
 
-			FunctiesManager fm = Factory.Maak<FunctiesManager>();
-			GroepenManager gm = Factory.Maak<GroepenManager>();
+			Debug.Assert(testData.HuidigGwj != null);
 
-			Functie f = gm.FunctieToevoegen(
-				testData.DummyGroep,
-				testData.NieuweFunctieNaam,
-				testData.NieuweFunctieCode,
-				1, 1,
-				LidType.Alles);
+			var veelGebruiktMock = new Mock<IVeelGebruikt>();
+			veelGebruiktMock.Setup(vgb => vgb.GroepsWerkJaarOphalen(testData.DummyGroep.ID)).Returns(testData.HuidigGwj);
+			Factory.InstantieRegistreren<IVeelGebruikt>(veelGebruiktMock.Object);
 
-			// Functie bewaren, zodat dummydao een ID toekent.
+			var mgr = Factory.Maak<FunctiesManager>();
 
-			f = fm.Bewaren(f);
+			// act
 
-			IEnumerable<Functie> functies = new Functie[] { f };
+			var result = mgr.Verwijderen(testData.TraditieFunctie, true);
 
-			// Act
+			// assert
 
-			fm.Toekennen(testData.LeiderJos, functies);
-			fm.Toekennen(testData.LidYvonne, functies);
+			// functie niet meer geldig
+			Assert.IsNotNull(result);
+			Assert.AreEqual(result.WerkJaarTot, testData.HuidigGwj.WerkJaar - 1);
 
-			// Assert
-
-			var problemen = fm.AantallenControleren(testData.HuidigGwj, functies);
-			Assert.AreNotEqual(problemen.Count(), 0);
+			// enkel het lid van dit werkJaar blijft over
+			Assert.AreEqual(result.Lid.Count, 1);
 		}
-
-		/// <summary>
-		/// Kijkt na of de verplichte aantallen genegeerd worden voor functies die niet geldig zijn
-		/// in het gegeven groepswerkjaar.
-		/// </summary>
-		[TestMethod]
-		public void IrrelevanteVerplichteFunctie()
-		{
-			// Arrange
-
-			Factory.ContainerInit();	// Container resetten alvorens dummydata te maken.
-			var testData = new DummyData();
-
-			var fm = Factory.Maak<FunctiesManager>();
-			var gm = Factory.Maak<GroepenManager>();
-
-			Functie f = gm.FunctieToevoegen(
-				testData.DummyGroep,
-				testData.NieuweFunctieNaam,
-				testData.NieuweFunctieCode,
-				1, 1,
-				LidType.Alles);	// pas volgend jaar geldig
-
-		    f.WerkJaarTot = testData.HuidigGwj.WerkJaar - 1; // vervallen functie
-
-			f.ID = testData.NieuweFunctieID;
-
-			// Jos krijgt alle nationaal bepaalde functies, zodat eventuele verplichte
-			// nationaal bepaalde functies OK zijn.
-			fm.Toekennen(testData.LeiderJos, fm.NationaalBepaaldeFunctiesOphalen());
-
-			// Act
-
-			var problemen = from p in fm.AantallenControleren(testData.HuidigGwj)
-			                where p.ID == testData.NieuweFunctieID
-			                select p;
-
-			// Assert
-
-			Assert.AreEqual(problemen.Count(), 0);
-		}
-
-		/// <summary>
-		/// Standaard 'AantallenControleren'.  Nakijken of rekening wordt gehouden
-		/// met nationaal bepaalde functies.
-		/// </summary>
-		[TestMethod]
-		public void OntbrekendeNationaalBepaaldeFuncties()
-		{
-			// Arrange
-
-			var testData = new DummyData();
-
-			FunctiesManager fm = Factory.Maak<FunctiesManager>();
-
-			// Act
-
-			var problemen = fm.AantallenControleren(testData.HuidigGwj);
-			// Het DummyFunctieDao voorziet nationaal bepaalde verplichte functies.
-
-			// Assert
-
-			Assert.AreNotEqual(problemen.Count(), 0);
-		}
-
-
-
-		/// <summary>
-		/// Test of regelementaire 'FunctieBewaren' geen exception oplevert.
-		/// </summary>
-		[TestMethod]
-		public void FunctieBewarenTest()
-		{
-			// Arrange
-
-			var testData = new DummyData();
-
-			FunctiesManager fm = Factory.Maak<FunctiesManager>();
-			GroepenManager gm = Factory.Maak<GroepenManager>();
-
-			Functie f = gm.FunctieToevoegen(
-				testData.DummyGroep,
-				testData.NieuweFunctieNaam,
-				testData.NieuweFunctieCode,
-				1, 0,
-				0);
-
-			// Act
-
-			f = fm.Bewaren(f);
-
-			// Assert
-
-			Assert.AreNotEqual(f.ID, 0);
-		}
-
-		/// <summary>
-		/// Test op exception bij poging tot bewaren van nationaal bepaalde functie.
-		/// </summary>
-		[TestMethod]
-		[ExpectedException(typeof(GeenGavException))]
-		public void NationaalBepaaldeFunctieBewarenTest()
-		{
-			// Arrange
-
-			var testData = new DummyData();
-
-			var fm = Factory.Maak<FunctiesManager>();
-
-			var f = fm.NationaalBepaaldeFunctiesOphalen().First();
-
-			// Act
-
-			f = fm.Bewaren(f);
-
-			// Assert
-
-			// Dit mogen we niet halen.
-			Assert.IsTrue(false);
-		}
-
-		/// <summary>
-		/// Testfuncties vervangen
-		/// </summary>
-		[TestMethod]
-		public void FunctiesVervangen()
-		{
-			// Arrange
-
-			var testData = new DummyData();
-
-			var fm = Factory.Maak<FunctiesManager>();
-			var gm = Factory.Maak<GroepenManager>();
-
-			Functie f = gm.FunctieToevoegen(
-				testData.DummyGroep,
-				testData.NieuweFunctieNaam,
-				testData.NieuweFunctieCode,
-				1, 0,
-				LidType.Alles);
-
-			// Het DummyDao kent een ID toe aan f.  (Voor DummyDao is dat OK, maar in echte situaties
-			// niet, omdat de nieuwe f niet gekoppeld zou zijn aan de groep.  Eigenlijk moeten we
-			// de groep bewaren, samen met zijn functies.)
-
-			f = fm.Bewaren(f);
-
-			var natBepFuncties = fm.NationaalBepaaldeFunctiesOphalen();
-			// we weten dat er minstens 2 nat. bepaalde functies zijn.
-			IEnumerable<Functie> functies = new Functie[] { f, natBepFuncties.First() };
-			fm.Toekennen(testData.LeiderJos, functies);
-
-			// Act
-
-			fm.Vervangen(testData.LeiderJos, natBepFuncties);
-
-			// Assert
-
-			Assert.IsTrue(testData.LeiderJos.Functie.Contains(natBepFuncties.First()));
-			Assert.IsTrue(testData.LeiderJos.Functie.Contains(natBepFuncties.Last()));
-			Assert.IsFalse(testData.LeiderJos.Functie.Contains(f));
-		}
-
 	}
-
 }
