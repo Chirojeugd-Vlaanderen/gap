465fc3291ff8fee0dacf4c726c29806380ade908
unknown <THAEPER@LBNL06686.corp.capgemini.com>
2012-06-09 03:21:44 +0200

    Aanpassingen om database van Prod naar Dev te krijgen

--- Solution/TestProjecten/Chiro.Gap.TestDbInfo/TestInfo.cs
+++ Solution/TestProjecten/Chiro.Gap.TestDbInfo/TestInfo.cs
@@ -9,36 +9,37 @@
 		// te plakken uit output van trunk/db/TestSetup/4_unit_tests.sql
 		// 
         public const int GROEPID = 1065;
-        public const int GELIEERDEPERSOONID = 225183;
-        public const int GELIEERDEPERSOON2ID = 225184;
-        public const int GELIEERDEPERSOON3ID = 225185;
-        public const int GELIEERDEPERSOON4ID = 225186;
-        public const int GELIEERDEPERSOON5ID = 225187;
+        public const int GELIEERDEPERSOONID = 227268;
+        public const int GELIEERDEPERSOON2ID = 227269;
+        public const int GELIEERDEPERSOON3ID = 227270;
+        public const int GELIEERDEPERSOON4ID = 227271;
+        public const int GELIEERDEPERSOON5ID = 227272;
         public const int MINAANTALGELPERS = 5;
-        public const int LID3ID = 421600;
-        public const int LID4ID = 421601;
-        public const int LID5ID = 421603;
-        public const int CATEGORIEID = 887;
+        public const int LID3ID = 424454;
+        public const int LID4ID = 424455;
+        public const int LID5ID = 424457;
+        public const int CATEGORIEID = 920;
         public const string CATEGORIECODE = "last";
-        public const int CATEGORIE2ID = 888;
-        public const int CATEGORIE3ID = 889;
-        public const int FUNCTIEID = 409;
+        public const int CATEGORIE2ID = 921;
+        public const int CATEGORIE3ID = 922;
+        public const int FUNCTIEID = 421;
         public const int AANTALINCATEGORIE = 1;
-        public const int AFDELING1ID = 5866;
+        public const int AFDELING1ID = 5872;
         public const int OFFICIELEAFDELINGID = 1;
-        public const int AFDELINGSJAAR1ID = 23209;
-        public const int AFDELING1VAN = 2003;
-        public const int AFDELING1TOT = 2004;
-        public const int AFDELING2ID = 5867;
-        public const int AFDELINGSJAAR2ID = 23210;
-        public const int AFDELING2VAN = 2003;
-        public const int AFDELING2TOT = 2004;
-        public const int AFDELING3ID = 5868;
+        public const int AFDELINGSJAAR1ID = 23266;
+        public const int AFDELING1VAN = 2003 ;
+        public const int AFDELING1TOT = 2004 ;
+        public const int AFDELING2ID = 5873;
+        public const int AFDELINGSJAAR2ID = 23267;
+        public const int AFDELING2VAN = 2003 ;
+        public const int AFDELING2TOT = 2004 ;
+        public const int AFDELING3ID = 5874;
         public const int ADRESID = 53;
-        public const int GROEPSWERKJAARID = 5033;
+        public const int GROEPSWERKJAARID = 5042;
         public const string ZOEKNAAM = "Bosmans";
         public const string GAV1 = "Yvonne";
         public const string GAV2 = "Yvette";
+
 		#endregion
 
 		
--- database/TestSetup/X_rechtendevs.sql
+++ database/TestSetup/X_rechtendevs.sql
@@ -1,17 +1,35 @@
 -- STAP X: gebruikersrechten devs
--- (nodig voor dev, niet voor tst)
+-- (enkel nodig voor dev, niet voor tst)
 
 use gap_dev
 go
 
-
-
+-- Merijn Gouwelose
 exec auth.spGebruikersRechtToekennen 'MJ /0108', 'KIPDORP\gouweme'
-exec auth.spGebruikersRechtToekennen 'BJ /0204', 'broes-laptop2\Broes'
+
+-- Bart B
 exec auth.spGebruikersRechtToekennen 'OG /1504', 'KIPDORP\booneba'
+
+-- Don't Know
 exec auth.spGebruikersRechtToekennen 'AG /0312', 'ATOMIUM\MDAAF68'
+
+-- Johan the man
 exec auth.spGebruikersRechtToekennen 'OM /2514', 'lap-jve2\johanv'
 exec auth.spGebruikersRechtToekennen 'MM /0813', 'lap-jve2\johanv'
 exec auth.spGebruikersRechtToekennen 'MG /0100', 'lap-jve2\johanv'
+
+-- Matthias Keustermans
 exec auth.spGebruikersRechtToekennen 'MJ /0106', 'kipdorp\keustma'
+
+-- Mathias De Park
 exec auth.spGebruikersRechtToekennen 'MJ /0106', 'Mathias-PC\Mathias'
+
+-- Tommy Haepers
+exec auth.spGebruikersRechtToekennen 'OM /2514', 'CORP\THaeper'
+exec auth.spGebruikersRechtToekennen 'OG /1504', 'CORP\THaeper'
+exec auth.spGebruikersRechtToekennen 'MG /0300', 'CORP\THaeper'
+
+-- Broes De Cat
+exec auth.spGebruikersRechtToekennen 'BJ /0204', 'broes-laptop2\Broes'
+exec auth.spGebruikersRechtToekennen 'MG /0200', 'broes-laptop2\Broes'
+exec auth.spGebruikersRechtToekennen 'BG /0200', 'broes-laptop2\Broes'
