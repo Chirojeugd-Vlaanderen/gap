3e0b6fa066f5f978211e91ffcd58b90f09722c89
vervljo <johan.vervloet@chiro.be>
2012-08-03 11:19:32 +0200

    Closes #1323. Dit was blijkbaar al in orde. Ik heb gauw een unit test gemaakt.

--- Solution/TestProjecten/Chiro.Gap.Workers.Test/LedenManagerTest.cs
+++ Solution/TestProjecten/Chiro.Gap.Workers.Test/LedenManagerTest.cs
@@ -73,7 +73,7 @@ namespace Chiro.Gap.Workers.Test
 
 
         /// <summary>
-        ///A test for InschrijvingVoorstellen
+        /// Test of 'LedenManager.InschrijvingVoorstellen' rekening houdt met het geslacht van een persoon.
         ///</summary>
         [TestMethod()]
         public void InschrijvingVoorstellenTest()
@@ -113,6 +113,58 @@ namespace Chiro.Gap.Workers.Test
 
             // Act
             
+            var actual = target.InschrijvingVoorstellen(gp, gwj, false);
+
+            // Assert
+
+            Assert.AreEqual(actual.AfdelingsJaarIDs[0], 2);
+        }
+
+        /// <summary>
+        /// Test of er een 'redelijke' afdeling wordt voorgesteld als er voor een persoon
+        /// geen afdeling wordt gevonden waarin die 'natuurlijk' past.
+        ///</summary>
+        [TestMethod()]
+        public void InschrijvingVoorstellenTest1()
+        {
+            // Arrange
+            // Ik maak een persoon, en een werkjaar met 2 afdelingen, zodanig dat
+            // de geboortedatum van die persoon buiten de afdelingen valt. We verwachten
+            // de afdeling die het meest logisch is (kleinste verschil met geboortedatum)
+
+            GelieerdePersoon gp = new GelieerdePersoon
+            {
+                Persoon =
+                    new Persoon
+                    {
+                        GeboorteDatum = new DateTime(1996, 03, 07),
+                        Geslacht = GeslachtsType.Vrouw,
+                    }
+            };
+
+            GroepsWerkJaar gwj = new GroepsWerkJaar();
+            gwj.AfdelingsJaar.Add(new AfdelingsJaar
+            {
+                ID = 1,
+                GeboorteJaarVan = 1999,
+                GeboorteJaarTot = 2000,
+                Geslacht = GeslachtsType.Gemengd,
+                OfficieleAfdeling = new OfficieleAfdeling()
+            });
+            gwj.AfdelingsJaar.Add(new AfdelingsJaar
+            {
+                ID = 2,
+                GeboorteJaarVan = 1997,
+                GeboorteJaarTot = 1998,
+                Geslacht = GeslachtsType.Gemengd,
+                OfficieleAfdeling = new OfficieleAfdeling()
+            });
+
+
+            LedenManager target = Factory.Maak<LedenManager>();
+
+            // Act
+
             var actual = target.InschrijvingVoorstellen(gp, gwj, false);
 
             // Assert
