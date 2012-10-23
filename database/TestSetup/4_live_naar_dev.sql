b9c136f2be53c06e5d779ab6ba1f24fa63ebc38e
Johan Vervloet <johan.vervloet@chiro.be>
2012-10-23 12:42:12 +0200

    Closes #1365. Ik vermoed dat het terug gaat werken...

--- database/TestSetup/4_live_naar_dev.sql
+++ database/TestSetup/4_live_naar_dev.sql
@@ -3,29 +3,12 @@
 -- Hier moeten alle aanpassingen komen aan de database die nodig zijn om de live-db
 -- om te zetten naar de db die gebruikt wordt in dev.
 
-USE gap_tst;
-
-ALTER TABLE lid.Lid ADD UitschrijfDatum DATETIME NULL;
+USE [master]
 GO
 
-UPDATE lid.Lid SET UitschrijfDatum = '2012-08-01' WHERE NonActief=1;
+ALTER DATABASE [Gap_Dev] SET COMPATIBILITY_LEVEL = 100
+GO
 
-DROP INDEX [IDX_Lid_EindeInstapPeriode_NonActief] ON [lid].[Lid]
+ALTER DATABASE [Gap_Dev] SET RECOVERY SIMPLE WITH NO_WAIT
+GO
 
-DROP INDEX [IDX_Lid_GroepsWerkJaarID_NonActief] ON [lid].[Lid]
-
-ALTER TABLE lid.Lid DROP COLUMN NonActief;
-
-ALTER TABLE lid.Lid ADD NonActief AS (CASE WHEN UitschrijfDatum IS NULL THEN CAST(0 AS BIT) ELSE CAST(1 AS BIT) END);
-
-CREATE NONCLUSTERED INDEX [IDX_Lid_EindeInstapPeriode_UitschrijfDatum] ON [lid].[Lid]
-(
-	[EindeInstapPeriode] ASC,
-	[UitschrijfDatum] ASC
-)
-
-CREATE NONCLUSTERED INDEX [IDX_Lid_GroepsWerkJaarID_UitschrijfDatum] ON [lid].[Lid]
-(
-	[GroepsWerkjaarID] ASC,
-	[UitschrijfDatum] ASC
-)
