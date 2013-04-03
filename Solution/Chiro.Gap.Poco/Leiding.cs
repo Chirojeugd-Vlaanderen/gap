eb31fb296776e7e4ceba6e64424101ca9df528bf
Johan Vervloet <johan.vervloet@gmail.com>
2013-04-03 21:47:50 +0200

    Refs #1425 - Notice toegevoegd aan cs-bestanden waar er nog geen stond

--- Solution/Chiro.Gap.Poco/Leiding.cs
+++ Solution/Chiro.Gap.Poco/Leiding.cs
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2008-2013 the GAP developers. See the NOTICE file at the 
+ * top-level directory of this distribution, and at
+ * https://develop.chiro.be/gap/wiki/copyright
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 using System.Collections.Generic;
 
 namespace Chiro.Gap.Poco.Model
