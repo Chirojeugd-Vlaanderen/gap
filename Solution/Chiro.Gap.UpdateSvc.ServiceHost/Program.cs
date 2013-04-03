f04389970598dcaaaefc6972cb52a4c3e84ad206
Johan Vervloet <johan.vervloet@gmail.com>
2013-04-03 21:51:43 +0200

    Refs #1425 - Bestaande copyrightnotices vervangen in cs-bestanden

--- Solution/Chiro.Gap.UpdateSvc.ServiceHost/Program.cs
+++ Solution/Chiro.Gap.UpdateSvc.ServiceHost/Program.cs
@@ -1,7 +1,20 @@
-﻿// <copyright company="Chirojeugd-Vlaanderen vzw">
-// Copyright (c) 2007-2012
-// Mail naar informatica@chiro.be voor alle info over deze broncode
-// </copyright>
+/*
+ * Copyright 2008-2013 the GAP developers. See the NOTICE file at the 
+ * top-level directory of this distribution, and at
+ * https://develop.chiro.be/gap/wiki/copyright
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 using System.ServiceProcess;
 
