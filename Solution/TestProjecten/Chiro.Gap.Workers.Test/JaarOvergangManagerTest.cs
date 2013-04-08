daf0b3fff0598b40929c622f3e50d2e3ce755f09
Johan Vervloet <johan.vervloet@chiro.be>
2013-04-08 16:59:54 +0200

    Refs #705 - Test op jaarovergang herschreven naar nieuwe backend.
    Hij failt nog, maar dat komt omdat de jaarovergang nog niet geimplementeerd is.

--- Solution/TestProjecten/Chiro.Gap.Workers.Test/JaarOvergangManagerTest.cs
+++ Solution/TestProjecten/Chiro.Gap.Workers.Test/JaarOvergangManagerTest.cs
@@ -16,15 +16,13 @@
  * limitations under the License.
  */
 ﻿using System;
-using System.Data.Objects.DataClasses;
-using Chiro.Gap.Domain;
-using Chiro.Gap.Dummies;
-using Chiro.Gap.Poco.Model;
-using Chiro.Gap.WorkerInterfaces;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+﻿using Chiro.Cdf.Ioc;
+﻿using Chiro.Gap.Domain;
+﻿using Chiro.Gap.Poco.Model;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Chiro.Gap.ServiceContracts.DataContracts;
 using System.Collections.Generic;
-using Moq;
 
 namespace Chiro.Gap.Workers.Test
 {
@@ -41,10 +39,11 @@ namespace Chiro.Gap.Workers.Test
 		//You can use the following additional attributes as you write your tests:
 		//
 		//Use ClassInitialize to run code before running the first test in the class
-		//[ClassInitialize()]
-		//public static void MyClassInitialize(TestContext testContext)
-		//{
-		//}
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            Factory.ContainerInit();
+        }
 		//
 		//Use ClassCleanup to run code after all tests in a class have run
 		//[ClassCleanup()]
@@ -66,69 +65,40 @@ namespace Chiro.Gap.Workers.Test
 		//
 		#endregion
 
-        AfdelingsJaar call(Afdeling afd1, OfficieleAfdeling ribbeloff, GroepsWerkJaar gwj2, int GeboorteJaarVan, int GeboorteJaarTot, GeslachtsType Geslacht)
-        {
-            var x = new AfdelingsJaar
-            {
-                Afdeling = afd1,
-                GeboorteJaarTot = GeboorteJaarTot,
-                GeboorteJaarVan = GeboorteJaarVan,
-                Geslacht = Geslacht,
-                GroepsWerkJaar = gwj2,
-                OfficieleAfdeling = ribbeloff
-            }
-            ; gwj2.AfdelingsJaar.Add(x); return x;
-        }
 
 		/// <summary>
-		///A test for JaarOvergangUitvoeren
-		///</summary>
+		/// Voert een jaarovergang uit, en kijkt na of het nieuwe werkjaar het jusite aantal afdelingsjaren bevat.
+		/// </summary>
 		[TestMethod()]                    
 		public void JaarOvergangUitvoerenTest()
 		{
-            //var ribbeloff = new OfficieleAfdeling {ID = 1, LeefTijdTot = 7, LeefTijdVan = 6, Naam = "Ribbel"};
+            // ARRANGE
 
-            //var groep = new ChiroGroep {ID = 10};
-            //var gwj = new GroepsWerkJaar {WerkJaar = 2010, Groep = groep};
-            //var gwj2 = new GroepsWerkJaar { WerkJaar = 2012, Groep = groep };
-            //var afdjaar1 = new AfdelingsJaar { ID = 1, GeboorteJaarVan = DateTime.Today.Year - 6, GeboorteJaarTot = DateTime.Today.Year - 7, OfficieleAfdeling = ribbeloff };
-            //var afd1 = new Afdeling {ID = 2, AfdelingsJaar = new EntityCollection<AfdelingsJaar>{afdjaar1}};
-            //afdjaar1.Afdeling = afd1;
-            //groep.Afdeling.Add(afd1);
-            //gwj.AfdelingsJaar.Add(afdjaar1);
+            // Vrij veel voorbereiding voor een vrij flauwe test.
 
-            //var newafdjaar = new AfdelingDetail { AfdelingID = afd1.ID, AfdelingsJaarID = afdjaar1.ID, GeboorteJaarVan = DateTime.Today.Year - 8, GeboorteJaarTot = DateTime.Today.Year - 10, OfficieleAfdelingID = ribbeloff.ID, Geslacht = GeslachtsType.Gemengd};
+            var ribbeloff = new OfficieleAfdeling { ID = 1, LeefTijdTot = 7, LeefTijdVan = 6, Naam = "Ribbel" };
 
-            //var groepdao = new Mock<IGroepenDao>(MockBehavior.Strict);
-            //var geldao = new Mock<IGelieerdePersonenDao>(MockBehavior.Strict);
+            var groep = new ChiroGroep { ID = 10, GroepsWerkJaar = new List<GroepsWerkJaar>()};
+            var gwj = new GroepsWerkJaar { WerkJaar = 2010, Groep = groep };
+            groep.GroepsWerkJaar.Add(gwj);
+            var afdjaar1 = new AfdelingsJaar { ID = 1, GeboorteJaarVan = 2003, GeboorteJaarTot = 2004, OfficieleAfdeling = ribbeloff };
+            var afd1 = new Afdeling { ID = 2, AfdelingsJaar = new List<AfdelingsJaar> { afdjaar1 } };
+            afdjaar1.Afdeling = afd1;
+            groep.Afdeling.Add(afd1);
+            gwj.AfdelingsJaar.Add(afdjaar1);
 
-            //var vgdao = new Mock<IVeelGebruikt>(MockBehavior.Strict);
-            //vgdao.Setup(vg => vg.GroepsWerkJaarIDOphalen(It.IsAny<int>())).Returns(gwj);
+            var newafdjaar = new AfdelingDetail { AfdelingID = afd1.ID, AfdelingsJaarID = afdjaar1.ID, GeboorteJaarVan = DateTime.Today.Year - 8, GeboorteJaarTot = DateTime.Today.Year - 10, OfficieleAfdelingID = ribbeloff.ID, Geslacht = GeslachtsType.Gemengd };
 
-            //var autmanag = new Mock<IAutorisatieManager>(MockBehavior.Strict);
-            //autmanag.Setup(vg => vg.IsSuperGav()).Returns(false);
-            //autmanag.Setup(vg => vg.IsGavGroep(It.IsAny<int>())).Returns(true);
+            // ACT
 
-            //var gm = new GroepenManager(groepdao.Object, geldao.Object, vgdao.Object, autmanag.Object, new DummySync());
-            //var cgmm = new Mock<IChiroGroepenManager>(MockBehavior.Strict);
-            //cgmm.Setup(e => e.Ophalen(10, ChiroGroepsExtras.AlleAfdelingen | ChiroGroepsExtras.GroepsWerkJaren)).Returns(groep);
+		    var target = Factory.Maak<JaarOvergangManager>();
+            var teActiveren = new List<AfdelingDetail> { newafdjaar };
+            target.JaarOvergangUitvoeren(teActiveren, groep.ID);
 
-            //var ajmm = new Mock<IAfdelingsJaarManager>(MockBehavior.Strict);
-            //ajmm.Setup(e => e.OfficieleAfdelingenOphalen()).Returns(new List<OfficieleAfdeling> {ribbeloff});
-            //ajmm.Setup(e => e.Aanmaken(afd1, ribbeloff, gwj2, newafdjaar.GeboorteJaarVan, newafdjaar.GeboorteJaarTot, newafdjaar.Geslacht)).Returns(call(afd1, ribbeloff, gwj2, newafdjaar.GeboorteJaarVan, newafdjaar.GeboorteJaarTot, newafdjaar.Geslacht));
+            // ASSERT
 
-            //var gwmm = new Mock<IGroepsWerkJaarManager>(MockBehavior.Strict);
-            //gwmm.Setup(e => e.RecentsteOphalen(groep.ID)).Returns(gwj);
-            //gwmm.Setup(e => e.RecentsteOphalen(groep.ID, GroepsWerkJaarExtras.Afdelingen)).Returns(gwj);
-            //gwmm.Setup(e => e.OvergangMogelijk(It.IsAny<DateTime>(), It.IsAny<int>())).Returns(true);
-            //gwmm.Setup(e => e.VolgendGroepsWerkJaarMaken(groep)).Returns(gwj2);
-            //gwmm.Setup(e => e.Bewaren(gwj2, GroepsWerkJaarExtras.Groep | GroepsWerkJaarExtras.Afdelingen)).Returns(gwj2);
-
-            //var target = new JaarOvergangManager(gm, cgmm.Object, ajmm.Object, gwmm.Object);
-            //var teActiveren = new List<AfdelingDetail> {newafdjaar};
-            //target.JaarOvergangUitvoeren(teActiveren, groep.ID);
-            //Assert.AreEqual(gwmm.Object.RecentsteOphalen(groep.ID, GroepsWerkJaarExtras.Afdelingen).AfdelingsJaar.Count, 1);
-            throw new NotImplementedException(NIEUWEBACKEND.Info);
+            Assert.AreEqual(groep.GroepsWerkJaar.Count, 2); 
+            Assert.AreEqual(groep.GroepsWerkJaar.OrderByDescending(gwjr => gwjr.WerkJaar).First().AfdelingsJaar.Count, 1);
 		}
 	}
 }
