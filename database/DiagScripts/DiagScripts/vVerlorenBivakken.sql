e58dae04c32496a58c276bf1187f72de8d029ddf
Johan Vervloet <johan.vervloet@chiro.be>
2013-06-03 16:18:36 +0200

    Refs #1534 - view in database die ontbrekende bivakken opspoort in Kipadmin

---
+++ database/DiagScripts/DiagScripts/vVerlorenBivakken.sql
@@ -0,0 +1,14 @@
+use gap_dev;
+go
+create view diag.vVerlorenBivakken as
+select cg.StamNr, g.naam as GroepsNaam, cg.Gemeente, u.naam as BivakNaam, u.UitstapID
+from biv.uitstap u
+join grp.groepswerkjaar gwj on u.groepswerkjaarid=gwj.groepswerkjaarid
+join grp.groep g on gwj.groepid=g.groepid
+join kipadmin_dev.grp.chirogroep cg on cg.stamnr=g.code collate SQL_Latin1_General_CP1_CI_AS
+left outer join kipadmin_dev.biv.bivakaangifte bo on cg.groepID = bo.groepID and bo.werkjaar=gwj.werkjaar
+-- gebruik hierboven 'bivakoverzicht', an dan heb je er meer.
+-- ik vermoed dat groepen waarbij geen contactpersoon/adres opgegeven is bij het bivak
+-- wel in bivakaangifte zitten, maar niet in bivakoverzicht.
+where gwj.werkjaar=2012 and u.IsBivak=1 and bo.werkjaar is null
+
