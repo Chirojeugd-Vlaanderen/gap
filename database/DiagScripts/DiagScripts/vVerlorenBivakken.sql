b459382fddf1382f069956affcb584f3c5162255
Johan Vervloet <johan.vervloet@chiro.be>
2013-06-06 11:15:58 +0200

    Refs #1534 - view is nu hopelijk wel correct.

--- database/DiagScripts/DiagScripts/vVerlorenBivakken.sql
+++ database/DiagScripts/DiagScripts/vVerlorenBivakken.sql
@@ -5,10 +5,10 @@ select cg.StamNr, g.naam as GroepsNaam, cg.Gemeente, u.naam as BivakNaam, u.Uits
 from biv.uitstap u
 join grp.groepswerkjaar gwj on u.groepswerkjaarid=gwj.groepswerkjaarid
 join grp.groep g on gwj.groepid=g.groepid
+join kipadmin_dev.dbo.huidigWerkJaar wj on gwj.WerkJaar = wj.WerkJaar
 join kipadmin_dev.grp.chirogroep cg on cg.stamnr=g.code collate SQL_Latin1_General_CP1_CI_AS
-left outer join kipadmin_dev.biv.bivakaangifte bo on cg.groepID = bo.groepID and bo.werkjaar=gwj.werkjaar
--- gebruik hierboven 'bivakoverzicht', an dan heb je er meer.
--- ik vermoed dat groepen waarbij geen contactpersoon/adres opgegeven is bij het bivak
--- wel in bivakaangifte zitten, maar niet in bivakoverzicht.
-where gwj.werkjaar=2012 and u.IsBivak=1 and bo.werkjaar is null
+left outer join kipadmin_dev.biv.bivakoverzicht bo on cg.groepID = bo.groepID and bo.werkjaar=gwj.werkjaar
+-- bivakoverzicht bevat normaal alle bivakken
+-- bivakaangifte is wat de delphi client toont.
+where u.IsBivak=1 and bo.werkjaar is null
 
