3314c98ad8806d53c4db085154ddb13473527835
Johan Vervloet <johan.vervloet@chiro.be>
2013-06-07 11:55:38 +0200

    Closes #1534 - probleem niet-doorgekomen bivakaangiftes gefixt
    Als een groep eerst een verkeerd jaar ingaf voor de bivakaangifte, dan werd die bivakaangifte
    geregistreerd in het verkeerde werkjaar. Als je dan daarna de fout verbetert, dan gaf dat problemen
    omdat de bivakaangifte dan naar een ander werkjaar moest verhuizen.

--- database/DiagScripts/DiagScripts/vVerlorenBivakken.sql
+++ database/DiagScripts/DiagScripts/vVerlorenBivakken.sql
@@ -7,8 +7,9 @@ join grp.groepswerkjaar gwj on u.groepswerkjaarid=gwj.groepswerkjaarid
 join grp.groep g on gwj.groepid=g.groepid
 join kipadmin_dev.dbo.huidigWerkJaar wj on gwj.WerkJaar = wj.WerkJaar
 join kipadmin_dev.grp.chirogroep cg on cg.stamnr=g.code collate SQL_Latin1_General_CP1_CI_AS
-left outer join kipadmin_dev.biv.bivakoverzicht bo on cg.groepID = bo.groepID and bo.werkjaar=gwj.werkjaar
--- bivakoverzicht is wat de delphi client toont
+left outer join kipadmin_dev.biv.bivakaangifte bo on cg.groepID = bo.groepID and bo.werkjaar=gwj.werkjaar
+-- bivakoverzicht is wat de delphi client toont (-> enkel aangiften met adres & contactpersoon)
 -- bivakaangifte zou moeten overeenkomen met uitstap in GAP.
-where u.IsBivak=1 and bo.werkjaar is null
+where u.IsBivak=1 and bo.werkjaar is null and u.DatumVan >= kipadmin_dev.dbo.Date(gwj.werkjaar, 9, 1)
+										  and u.DatumVan <= kipadmin_dev.dbo.Date(gwj.werkjaar + 1, 9, 1)
 
