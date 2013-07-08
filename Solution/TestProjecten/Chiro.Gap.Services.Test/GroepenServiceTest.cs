4a7e230599be5e55fc1ea4d96053d96604329e6d
Johan Vervloet <johan.vervloet@chiro.be>
2013-07-08 12:26:22 +0200

    Refs #705 - fout in unit test
    De unit test die moet controleren op kleuters, mag niet afhankelijk zijn
    van de tijd. Dat is bij deze in orde gebracht.

--- Solution/TestProjecten/Chiro.Gap.Services.Test/GroepenServiceTest.cs
+++ Solution/TestProjecten/Chiro.Gap.Services.Test/GroepenServiceTest.cs
@@ -710,6 +710,13 @@ namespace Chiro.Gap.Services.Test
 
             // dependency injection
 
+            // We prutsen wat, zodat het volgende werkjaar altijd 2012 is.
+            var groepsWerkJarenManagerMock = new Mock<IGroepsWerkJarenManager>();
+            groepsWerkJarenManagerMock.Setup(src => src.NieuweWerkJaar(groep.ID)).Returns(2012);
+            groepsWerkJarenManagerMock.Setup(src => src.OvergangMogelijk(It.IsAny<DateTime>(), It.IsAny<int>())).Returns(true);
+            Factory.InstantieRegistreren(groepsWerkJarenManagerMock.Object);
+
+            // Data access faken
             var dummyRepositoryProvider = new Mock<IRepositoryProvider>();
             dummyRepositoryProvider.Setup(src => src.RepositoryGet<Groep>())
                                    .Returns(new DummyRepo<Groep>(new List<Groep> { groep }));
