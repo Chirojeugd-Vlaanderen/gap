bac0918a202cb007a0a0908bee1f67c7f4070441
Johan Vervloet <johan.vervloet@chiro.be>
2013-07-18 21:12:13 +0200

    Refs #1600 - unit test: geupdatete afdelingen gaan niet meer naar Kipadmin.

---
+++ Solution/TestProjecten/Chiro.Gap.Sync.Test/LedenSyncTest.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using Chiro.Cdf.Ioc;
+using Chiro.Gap.Sync;
+using Chiro.Kip.ServiceContracts;
+using Chiro.Kip.ServiceContracts.DataContracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Chiro.Gap.Poco.Model;
+using Moq;
+using Persoon = Chiro.Gap.Poco.Model.Persoon;
+
+namespace Chiro.Gap.Sync.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for LedenSyncTest and is intended
+    ///to contain all LedenSyncTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class LedenSyncTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            MappingHelper.MappingsDefinieren();
+        }
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            Factory.ContainerInit();
+        }
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for AfdelingenUpdaten
+        ///</summary>
+        [TestMethod()]
+        public void AfdelingenUpdatenTest()
+        {
+            // ARRANGE
+
+            var kipSyncMock = new Mock<ISyncPersoonService>();
+            kipSyncMock.Setup(src => src.AfdelingenUpdaten(It.IsAny<Kip.ServiceContracts.DataContracts.Persoon>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<IEnumerable<AfdelingEnum>>())).Verifiable();
+            Factory.InstantieRegistreren(kipSyncMock.Object);
+
+            var lid = new Kind
+                          {
+                              AfdelingsJaar = new AfdelingsJaar {OfficieleAfdeling = new OfficieleAfdeling {ID = 1}},
+                              GroepsWerkJaar = new GroepsWerkJaar {Groep = new ChiroGroep()},
+                              GelieerdePersoon = new GelieerdePersoon {Persoon = new Persoon()}
+                          };
+
+            // ACT
+
+            var target = new LedenSync();
+            target.AfdelingenUpdaten(lid);
+
+            // ASSERT
+
+            kipSyncMock.VerifyAll();
+        }
+    }
+}
