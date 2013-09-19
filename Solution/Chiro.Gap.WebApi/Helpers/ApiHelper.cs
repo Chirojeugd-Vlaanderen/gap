7b77bd15eebf7e3a594a69b65abc4b62b42185e9
Johan Vervloet <johan.vervloet@chiro.be>
2013-09-19 17:20:29 +0200

    Ik had problemen met het ophalen van de username.

--- Solution/Chiro.Gap.WebApi/Helpers/ApiHelper.cs
+++ Solution/Chiro.Gap.WebApi/Helpers/ApiHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using Chiro.Adf.ServiceModel;
@@ -12,10 +13,12 @@ namespace Chiro.Gap.WebApi.Helpers
     {
         public static GroepsWerkJaar GetGroepsWerkJaar(ChiroGroepEntities context)
         {
+            Debug.Assert(HttpContext.Current.Request.LogonUserIdentity != null);
+
             // Selector voor de geldige gebruikersrechten
             Func<GebruikersRecht, bool> geldigeRechten =
                 g =>
-                String.Equals(g.Gav.Login, HttpContext.Current.User.Identity.Name, StringComparison.CurrentCultureIgnoreCase) &&
+                String.Equals(g.Gav.Login, HttpContext.Current.Request.LogonUserIdentity.Name, StringComparison.CurrentCultureIgnoreCase) &&
                 (g.VervalDatum == null || g.VervalDatum > DateTime.Now);
 
             // Haal het eerste geldige recht op
