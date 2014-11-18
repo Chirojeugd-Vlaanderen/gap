6aef59d4677bc8717d2e4e75e9f2b7da34a76f17
Johan Vervloet <johanv@johanv.org>
2014-11-18 20:34:30 +0100

    Refs #1750 - HelloWorld-API'tje.

---
+++ Solution/Chiro.Gap.UpdateApi/PersoonModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chiro.Gap.UpdateApi
+{
+    public class PersoonModule: Nancy.NancyModule
+    {
+        public PersoonModule()
+        {
+            Get["/"] = _ => "Hello World!";
+        }
+    }
+}
