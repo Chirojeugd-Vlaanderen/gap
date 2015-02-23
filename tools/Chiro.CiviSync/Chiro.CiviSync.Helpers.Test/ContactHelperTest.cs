73c1ec79d92c1d1dfc2ba46fd080d2e7eda5628c
Johan Vervloet <johanv@johanv.org>
2015-02-23 17:22:05 +0100

    Refs #3419 - Unit test (crash civisync lid maken ongeldig ad-nummer).

---
+++ tools/Chiro.CiviSync/Chiro.CiviSync.Helpers.Test/ContactHelperTest.cs
@@ -0,0 +1,70 @@
+﻿/*
+   Copyright 2015 Chirojeugd-Vlaanderen vzw
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+using System;
+using Chiro.Cdf.Ioc;
+using Chiro.Cdf.ServiceHelper;
+using Chiro.CiviCrm.Api;
+using Chiro.CiviCrm.Api.DataContracts.Entities;
+using Chiro.CiviCrm.Api.DataContracts.Requests;
+using Chiro.CiviSync.Services.Test;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Chiro.CiviSync.Helpers.Test
+{
+    [TestClass]
+    public class ContactHelperTest
+    {
+        private Mock<ICiviCrmApi> _civiApiMock;
+        private readonly DateTime _vandaagZogezegd = new DateTime(2015, 2, 6);
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            _civiApiMock = TestHelper.IocOpzetten(_vandaagZogezegd);
+        }
+
+        /// <summary>
+        /// Persoon met recentste lid mag niet crashen als de API geen persoon
+        /// oplevert.
+        /// </summary>
+        [TestMethod]
+        public void PersoonMetRecentsteLidOngeldigAd()
+        {
+            // ARRANGE
+
+            // Als de API niets vindt, levert GetSingle een leeg object.
+            _civiApiMock.Setup(
+                src => src.ContactGetSingle(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<ContactRequest>()))
+                .Returns(new Contact());
+
+            // ContactHelper vraagt de API-keys bij constructie. Dat is misschien niet
+            // zo'n goed idee. Maar voorlopig doe ik het zo dus ook in deze test.
+
+            var serviceHelper = Factory.Maak<ServiceHelper>();
+            var contactHelper = new ContactHelper(serviceHelper, String.Empty, String.Empty);
+
+            // ACT
+
+            var result = contactHelper.PersoonMetRecentsteLid(2, 3);
+
+            // ASSERT
+
+            Assert.IsNull(result);
+        }
+    }
+}
