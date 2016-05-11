14bbb6a4b534a2f1ddbdae71f9f387cbea6068c7
Johan Vervloet <johanv@johanv.org>
2016-05-11 12:38:35 +0200

    Closes #5079 - FixAnomalies haalt niet meer alle leden in 1 keer op.

--- tools/Chiro.Gap.FixAnomalies/Chiro.Gap.FixAnomalies/Properties/Settings.Designer.cs
+++ tools/Chiro.Gap.FixAnomalies/Chiro.Gap.FixAnomalies/Properties/Settings.Designer.cs
@@ -46,5 +46,17 @@ namespace Chiro.Gap.FixAnomalies.Properties {
                 this["SiteKey"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("60000")]
+        public int LedenBlokGrootte {
+            get {
+                return ((int)(this["LedenBlokGrootte"]));
+            }
+            set {
+                this["LedenBlokGrootte"] = value;
+            }
+        }
     }
 }
