ad8ef3cfa46fd07008086303d53478b8b1ebe9b9
Johan Vervloet <johanv@johanv.org>
2016-12-20 17:38:38 +0100

    Refs #5594 - One changescript to rule them all.

--- database/changes/CHANGES.sql
+++ database/changes/CHANGES.sql
@@ -16,30 +16,212 @@
 
 -- Voeg hier de wijzigingen toe die moeten gebeuren aan de database.
 
--- Een view voor actieve abonnementen (zie #5463)
--- Drop view als hij al bestaat, dan kunnen we hem terug maken.
-IF OBJECT_ID('diag.vActiefAbonnement', 'V') IS NOT NULL
-    DROP VIEW diag.vActiefAbonnement;
+-- replace old tables with new tables.
+
+CREATE TABLE auth.GebruikersRechtv2(
+	GebruikersRechtV2ID int IDENTITY(1,1) NOT NULL,
+	PersoonID INT NOT NULL,
+	GroepID INT NOT NULL,
+	VervalDatum DATETIME NULL,
+	PersoonsPermissies INT NOT NULL DEFAULT 0,
+	GroepsPermissies INT NOT NULL DEFAULT 0,
+	AfdelingsPermissies INT NOT NULL DEFAULT 0,
+	IedereenPermissies INT NOT NULL DEFAULT 0,
+	Versie TIMESTAMP NULL,
+ CONSTRAINT PK_GebruikersrechtV2 PRIMARY KEY(GebruikersRechtV2ID)
+);
 GO
 
-CREATE VIEW diag.vActiefAbonnement AS
--- Voor 't gemak zitten hier dubbels bij.
-SELECT DISTINCT ab.AbonnementID, gp.PersoonID, ab.Type, p.AdNummer, huidigwj.WerkJaar 
-FROM abo.Abonnement ab
-JOIN
-(
--- We gaan er even vanuit dat recente groepswerkjaren een hoger ID
--- hebben dan oude groepswerkjaren. We mogen dat eigenlijk niet doen.
--- Maar we doen het toch.
-SELECT
-MAX(gwj.GroepsWerkJaarID) AS GroepsWerkJaarID,
-MAX(gwj.WerkJaar) AS WerkJaar,
-gwj.GroepID
-FROM grp.GroepsWerkJaar gwj
-GROUP BY gwj.GroepID
-) huidigwj ON ab.GroepsWerkjaarID = huidigwj.GroepsWerkJaarID
-JOIN grp.Groep g ON huidigwj.GroepID = g.GroepID
-JOIN pers.GelieerdePersoon gp on ab.GelieerdePersoonID = gp.GelieerdePersoonID
-JOIN pers.Persoon p on gp.PersoonID = p.PersoonID
-WHERE g.StopDatum IS NULL;
+ALTER TABLE auth.GebruikersRechtV2 ADD CONSTRAINT FK_GebruikersRechtV2_Persoon FOREIGN KEY(PersoonID)
+REFERENCES pers.Persoon(PersoonID);
 GO
+
+ALTER TABLE auth.GebruikersRechtV2 ADD CONSTRAINT FK_GebruikersRechtV2_Groep FOREIGN KEY(GroepID)
+REFERENCES grp.Groep(GroepID);
+GO
+
+CREATE UNIQUE INDEX AK_GebruikersRechtV2_PersoonID_GroepID ON auth.GebruikersRechtV2(PersoonID, GroepID);
+GO
+
+INSERT INTO auth.GebruikersRechtV2(PersoonID, GroepID, VervalDatum, GroepsPermissies, IedereenPermissies)
+SELECT gs.PersoonID, gr.GroepID, max(gr.VervalDatum) AS VervalDatum, 3 as GroepsPermissies, 3 as IedereenPermissies
+FROM auth.GebruikersRecht gr
+JOIN auth.Gav gav on gr.GavID = gav.GavID
+JOIN auth.GavSchap gs on gav.GavID = gs.GavID
+GROUP BY gs.PersoonID, gr.GroepID
+GO
+
+CREATE INDEX IDX_GebruikersRechtV2_GroepID ON auth.GebruikersRechtV2(GroepID, VervalDatum) INCLUDE(PersoonID);
+GO
+
+CREATE INDEX IDX_GebruikersRechtV2_PersoonID ON auth.GebruikersRechtV2(PersoonID, VervalDatum) INCLUDE(GroepID);
+GO
+
+
+DROP TABLE auth.GavSchap;
+GO
+DROP TABLE auth.GebruikersRecht;
+GO
+DROP TABLE auth.Gav;
+GO
+
+-- procedures, vooral voor intranet/extranet
+
+CREATE PROCEDURE [auth].[spGebruikersRechtToekennenAd]
+(
+	@stamNr VARCHAR(10)
+	, @adNr int
+)
+AS
+-- Doel: Geeft login gebruikersrecht op gegeven groep
+-- Als de persoon ooit gebruikersrecht had, wordt dit gewoon verlengd.
+BEGIN
+	-- bepaal de vervaldatum.
+	declare @datum datetime
+			, @jaar varchar(4)
+
+	-- Vanaf juli gaan we ervan uit dat de login voor degene is
+	-- die het volgende werkjaar de groepsadministratie zal regelen.
+	IF MONTH(getdate()) >= 7
+		set @jaar = cast(year(getdate()) + 1 as varchar)
+	ELSE
+		set @jaar = cast(year(getdate()) as varchar)
+
+	set @datum =  @jaar + '-11-01';
+
+	INSERT INTO auth.GebruikersRechtV2(PersoonID, GroepID, VervalDatum, GroepsPermissies, IedereenPermissies)
+	SELECT
+			p.PersoonID, g.GroepID, @datum, 3, 3
+	FROM
+			pers.Persoon p
+			, grp.Groep g
+	WHERE
+			p.AdNummer=@adNr
+			AND g.Code=@stamnr
+			AND NOT EXISTS (SELECT 1 FROM auth.GebruikersRechtV2 gr
+							WHERE gr.PersoonID = p.PersoonID AND gr.GroepID = g.GroepID)
+
+	IF @@ROWCOUNT <= 0
+	BEGIN
+		RAISERROR('Dat StamNr is niet gevonden in de tabel Groep', 16, 1)
+	END
+END
+
+GO
+
+CREATE PROCEDURE auth.spAlleGebruikersRechtenOntnemenAd
+(
+	@adNr INT
+)
+AS
+-- Doel: Ontneemt gebruikersrecht op alle gekoppelde groepen
+BEGIN
+    UPDATE gr
+	SET VervalDatum = DateAdd(day, -1, getdate())
+	FROM auth.Gebruikersrechtv2 gr 
+	JOIN pers.Persoon p on gr.PersoonID= p.PersoonID
+	WHERE p.AdNummer = @adNr
+END
+GO
+
+
+CREATE procedure auth.spGebruikersRechtenOphalenAd
+(
+	@adNr INT
+)
+AS
+-- Doel: gegevens ophalen van de groepen waar de persoon als GAV aan gekoppeld is
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+			g.GroepID
+			, Code AS StamNr
+			, g.Naam
+			, Plaats
+			, gr.Vervaldatum
+	FROM
+			grp.Groep g
+			LEFT OUTER JOIN grp.Chirogroep cg
+				ON g.GroepID = cg.ChiroGroepID
+			INNER JOIN auth.GebruikersrechtV2 gr
+				ON g.GroepID = gr.GroepID
+			INNER JOIN pers.Persoon gav
+				ON gr.PersoonID = gav.PersoonID
+	WHERE
+			gav.AdNummer = @adNr
+			AND gr.GroepsPermissies = 3 AND gr.IedereenPermissies = 3
+END
+
+GO
+
+
+CREATE procedure [auth].[spGebruikersRechtenPerGroepOphalenAd]
+(
+	@stamnr varchar(8)
+)
+AS
+-- Doel: Personen ophalen met GAV-rechten op de groep
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT
+			gav.AdNummer
+			, gr.Vervaldatum
+	FROM
+			grp.Groep g
+			INNER JOIN auth.GebruikersrechtV2 gr
+				ON g.GroepID = gr.GroepID
+			INNER JOIN pers.Persoon gav
+				ON gr.PersoonID = gav.PersoonID
+	WHERE
+			Code = @stamnr
+			AND gr.Vervaldatum > dateadd(month, -3, getdate())
+			AND gr.GroepsPermissies = 3 AND gr.IedereenPermissies = 3
+END
+GO
+
+
+CREATE PROCEDURE auth.spGebruikersRechtOntnemenAd
+(
+	@stamNr VARCHAR(10)
+	, @adNr INT
+)
+AS
+-- Doel: Ontneemt gebruikersrecht op gegeven groep
+BEGIN
+    UPDATE gr
+	SET VervalDatum = DateAdd(day, -1, getdate())
+	FROM grp.Groep g JOIN auth.GebruikersrechtV2 gr ON g.GroepID = gr.GroepID
+					JOIN pers.Persoon gav ON gr.PersoonID = gav.PersoonID
+	WHERE g.Code=@stamNr AND gav.AdNummer=@adNr
+END
+GO
+
+
+-- procedures voor dev (moet weg uit live db)
+
+
+CREATE PROCEDURE [auth].[spWillekeurigeGroepToekennenAd]
+(
+	@adNr int,
+	@naam varchar(max),
+	@voornaam varchar(max),
+	@geboorteDatum DateTime,
+	@geslacht int -- 1 man, 2 vrouw
+)
+AS
+-- Doel: Kent rechten toe aan persoon met gegeven AD-nummer.
+-- Als er geen gebruiker is met gegeven AD-nummer, wordt die gemaakt op basis
+-- van meegeleverde naam en voornaam.
+BEGIN
+	DECLARE @stamnr as varchar(10);
+
+	insert into pers.persoon(AdNummer, Naam, VoorNaam, GeboorteDatum, Geslacht)
+	select @adNr, @naam, @voornaam, @geboortedatum, @geslacht
+	where not exists (select adNummer from pers.Persoon where adNummer=@adnr);
+
+	set @stamnr = (select top 1 g.Code from grp.Groep g where g.StopDatum is null order by newid());
+	exec auth.spGebruikersRechtToekennenAd @stamnr, @adNr
+END
+
