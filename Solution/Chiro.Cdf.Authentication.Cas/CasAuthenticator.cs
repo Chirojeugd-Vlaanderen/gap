42088626f0485812f1fe530250b6cc8869f7e400
Johan Vervloet <johanv@johanv.org>
2017-01-16 22:28:16 +0100

    Refs #3771 - Gauw een cas authenticator.

---
+++ Solution/Chiro.Cdf.Authentication.Cas/CasAuthenticator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chiro.Cdf.Authentication.Cas
+{
+    public class CasAuthenticator: IAuthenticator
+    {
+        public UserInfo WieBenIk()
+        {
+            var attributen = System.Web.HttpContext.Current.Session["userAttributeTable"];
+            throw new NotImplementedException();
+        }
+    }
+}
