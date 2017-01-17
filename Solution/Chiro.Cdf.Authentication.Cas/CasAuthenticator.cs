749bbf0b8affa74f2398c1cd9d0bdaee71a0d95a
Johan Vervloet <johanv@johanv.org>
2017-01-17 16:07:15 +0100

    Closes #3771 - W00T W00T! GAP en CAS!

--- Solution/Chiro.Cdf.Authentication.Cas/CasAuthenticator.cs
+++ Solution/Chiro.Cdf.Authentication.Cas/CasAuthenticator.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/*
+ * Copyright 2017 Chirojeugd-Vlaanderen vzw. See the NOTICE file at the 
+ * top-level directory of this distribution, and at
+ * https://gapwiki.chiro.be/copyright
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using DotNetCasClient.Security;
 
 namespace Chiro.Cdf.Authentication.Cas
 {
+    /// <summary>
+    /// Haalt het AD-nummer op uit de CasPrincipal.
+    /// </summary>
     public class CasAuthenticator: IAuthenticator
     {
         public UserInfo WieBenIk()
         {
-            var attributen = System.Web.HttpContext.Current.Session["userAttributeTable"];
-            throw new NotImplementedException();
+            var principal = System.Web.HttpContext.Current.User as CasPrincipal;
+            return new UserInfo()
+            {
+                AdNr = int.Parse(principal.Assertion.Attributes["cas:ad_nummer"].First())
+            };
         }
     }
 }
