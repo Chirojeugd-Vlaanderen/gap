a62b55b5cd4dd75b7a2a3a5b94d0194b440e4615
Johan Vervloet <johanv@johanv.org>
2017-02-01 11:27:07 +0100

    TestAdNr moet app setting zijn.
    Anders krijg je problemen met de CasAuthenticatorStaging.

--- Solution/Chiro.Cdf.Authentication.DevDummy/Properties/Settings.Designer.cs
+++ Solution/Chiro.Cdf.Authentication.DevDummy/Properties/Settings.Designer.cs
@@ -23,15 +23,12 @@ namespace Chiro.Cdf.Authentication.Dev.Properties {
             }
         }
         
-        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("18")]
         public int TestAdNr {
             get {
                 return ((int)(this["TestAdNr"]));
-            }
-            set {
-                this["TestAdNr"] = value;
             }
         }
         
