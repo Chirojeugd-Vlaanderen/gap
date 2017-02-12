05620e147f0253b21b735bbfa662ec477d30c33e
Johan Vervloet <johanv@johanv.org>
2017-02-12 18:59:58 +0100

    Refs #3283 - oauth poc.

---
+++ Solution/Chiro.Gap.Api/AuthContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Chiro.Gap.Api
+{
+    /// <summary>
+    /// Entity-Framework hocus-pocus voor gebruikers.
+    /// 
+    /// De bedoeling is dat we dit verschuiven naar de GAP-backend.
+    /// </summary>
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
---
+++ Solution/Chiro.Gap.Api/Controllers/LedenController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Chiro.Gap.Api.Controllers
+{
+    [RoutePrefix("api/Leden")]
+    public class LedenController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            // Return dummy data.
+            return Ok(Lid.CreateLeden());
+        }
+
+    }
+
+    #region Helpers
+
+    public class Lid
+    {
+        public int AdNummer { get; set; }
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public string[] Afdelingen { get; set; }
+        public bool IsLeiding { get; set; }
+
+        public static List<Lid> CreateLeden()
+        {
+            List<Lid> Ledenlijst = new List<Lid>
+            {
+                new Lid {AdNummer = 1, Afdelingen = new[] {"SP"}, Naam = "Mallezie", Voornaam = "Tim", IsLeiding = true},
+                new Lid
+                {
+                    AdNummer = 2,
+                    Afdelingen = new[] {"RAK"},
+                    Naam = "Severs",
+                    Voornaam = "Paul",
+                    IsLeiding = false
+                }
+            };
+
+            return Ledenlijst;
+        }
+    }
+
+    #endregion
+}
