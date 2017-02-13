94336812af645b115e4e2c72c4e4ae7cf62da80a
Johan Vervloet <johanv@johanv.org>
2017-02-13 11:46:08 +0100

    Refs #5634 - POC om AD-nummer terug op te halen
    Nu nog via API account/hello.

--- Solution/Chiro.Gap.Api/Controllers/AccountController.cs
+++ Solution/Chiro.Gap.Api/Controllers/AccountController.cs
@@ -1,5 +1,28 @@
-﻿// Source: http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/
+﻿/*
+ * Original code copied from
+ * http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/
+ * 
+ * Extra controller actions 
+ * Copyright 2016 Chirojeugd-Vlaanderen vzw. See the NOTICE file at the 
+ * top-level directory of this distribution, and at
+ * https://gapwiki.chiro.be/copyright
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+using System;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Chiro.Gap.Api.Models;
@@ -37,6 +60,17 @@ namespace Chiro.Gap.Api.Controllers
             }
 
             return Ok();
+        }
+
+        [Authorize]
+        [Route("Hello")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult Hello()
+        {
+            var claims = ((ClaimsIdentity) User.Identity).Claims;
+            int adnr = int.Parse((from c in claims where c.Type == "adnr" select c.Value).First());
+            string userName = User.Identity.GetUserName();
+            return Ok(String.Format("Hello {0} ({1})", userName, adnr));
         }
 
         protected override void Dispose(bool disposing)
