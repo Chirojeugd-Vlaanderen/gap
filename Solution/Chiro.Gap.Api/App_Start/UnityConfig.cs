ea3f99da41e2e41154ea257321ada6062d8e5445
Johan Vervloet <johanv@johanv.org>
2017-02-13 15:56:16 +0100

    Unity poging.

--- Solution/Chiro.Gap.Api/App_Start/UnityConfig.cs
+++ Solution/Chiro.Gap.Api/App_Start/UnityConfig.cs
@@ -1,6 +1,5 @@
+using Chiro.Cdf.Ioc.Factory;
 using Microsoft.Practices.Unity;
-using Microsoft.Practices.Unity.Configuration;
-using System.Configuration;
 using System.Web.Http;
 using Unity.WebApi;
 
@@ -10,32 +9,15 @@ namespace Chiro.Gap.Api
     {
         public static void RegisterComponents(HttpConfiguration config)
         {
-			var container = new UnityContainer();
-
+            // FIXME! Code smell. We moeten hier een IDependencyResolver maken die werkt met onze Factory.
+            Factory.ContainerInit();
+            var container = Factory.ContainerGet();
+            
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-
+            
             // e.g. container.RegisterType<ITestService, TestService>();
-
-            var section = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
-
-            // Als section null is, is er geen unity-configuratie in app.config of web.config.
-            // Op zich geen probleem, want de configuratie kan ook at runtime
-
-            if (section != null)
-            {
-                // Als de toepassing hierop crasht, kijk dan je config file na:
-                // * Heb je bij je types ook de assembly's vermeld waarin ze gedefinieerd zijn?
-                // * Bevat je project referenties naar al die assemblies?
-                // * Hernoemde je onlangs interfaces of implementaties?
-
-                // Wat ook kan helpen, is de ongebruikte referenties uit je project verwijderen,
-                // alsook de referenties nodig voor dependency injection, en die dan allemaal
-                // terugleggen.
-
-                section.Configure(container);
-            }
-
+            
             config.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
