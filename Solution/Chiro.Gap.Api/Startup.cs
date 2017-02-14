1354cbabc29f8d0afca712fdcfb42e6a6cd85bf0
Johan Vervloet <johanv@johanv.org>
2017-02-14 10:46:08 +0100

    De Startup class was zijn namespace kwijt.

--- Solution/Chiro.Gap.Api/Startup.cs
+++ Solution/Chiro.Gap.Api/Startup.cs
@@ -1,38 +1,42 @@
 ﻿// Source: http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/
+
 using System;
 using System.Web.Http;
-using Chiro.Gap.Api;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
 
-public class Startup
+[assembly: OwinStartup(typeof(Chiro.Gap.Api.Startup))]
+namespace Chiro.Gap.Api
 {
-    public void Configuration(IAppBuilder app)
+    public class Startup
     {
-        HttpConfiguration config = new HttpConfiguration();
-
-        ConfigureOAuth(app);
-        UnityConfig.RegisterComponents(config);
-        MappingHelper.CreateMappings();
-        WebApiConfig.Register(config);
-        app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
-        app.UseWebApi(config);
-    }
-
-    public void ConfigureOAuth(IAppBuilder app)
-    {
-        OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+        public void Configuration(IAppBuilder app)
         {
-            AllowInsecureHttp = true,
-            TokenEndpointPath = new PathString("/token"),
-            AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
-            Provider = new SimpleAuthorizationServerProvider()
-        };
+            HttpConfiguration config = new HttpConfiguration();
 
-        // Token Generation
-        app.UseOAuthAuthorizationServer(OAuthServerOptions);
-        app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+            ConfigureOAuth(app);
+            UnityConfig.RegisterComponents(config);
+            MappingHelper.CreateMappings();
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
 
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
     }
 }
